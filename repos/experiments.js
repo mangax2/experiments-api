be71ba30cf3629142e3b544e649489661f62d0e1
kmccl <kyle.mcclendon@monsanto.com>
2016-09-10 23:41:53 -0500

    uncommented some (2)

--- repos/experiments.js
+++ repos/experiments.js
@@ -1,27 +1,27 @@
-// module.exports = (rep, pgp) => {
-//     return {
-//         repository: () => {
-//             return rep
-//         },
-//
-//         find: (id) => {
-//             return rep.oneOrNone('SELECT * FROM experiments WHERE id = $1', id)
-//         },
-//
-//         all: () => {
-//             return rep.any('SELECT * FROM experiments')
-//         },
-//
-//         create: (t, experimentObj) => {
-//             return t.one("insert into experiments( name,status)  values('" + experimentObj.name + "' , '" + experimentObj.status + "' ) RETURNING id", (id) => { return id })
-//         },
-//
-//         update: (t, id, experimentObj) => {
-//             return t.one("UPDATE experiments SET name='" + experimentObj.name + "', status='" + experimentObj.status + "' WHERE id=" + id + " RETURNING *", (exp) => { return exp})
-//         },
-//
-//         delete: (t, id) => {
-//             return t.one('delete from experiments where id=" + id + " RETURNING id')
-//         }
-//     }
-// }
+module.exports = (rep, pgp) => {
+    return {
+        repository: () => {
+            return rep
+        },
+
+        find: (id) => {
+            return rep.oneOrNone('SELECT * FROM experiments WHERE id = $1', id)
+        },
+
+        all: () => {
+            return rep.any('SELECT * FROM experiments')
+        },
+
+        create: (t, experimentObj) => {
+            return t.one("insert into experiments( name,status)  values('" + experimentObj.name + "' , '" + experimentObj.status + "' ) RETURNING id", (id) => { return id })
+        },
+
+        update: (t, id, experimentObj) => {
+            return t.one("UPDATE experiments SET name='" + experimentObj.name + "', status='" + experimentObj.status + "' WHERE id=" + id + " RETURNING *", (exp) => { return exp})
+        },
+
+        delete: (t, id) => {
+            return t.one('delete from experiments where id=" + id + " RETURNING id')
+        }
+    }
+}

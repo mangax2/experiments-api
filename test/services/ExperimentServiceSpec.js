138fa42a362b58008c4d7b80acafc70872382be2
kprat1 <kamaraju.prathi@monsanto.com>
2016-10-06 11:34:20 -0500

    Stubbed transaction

--- test/services/ExperimentServiceSpec.js
+++ test/services/ExperimentServiceSpec.js
@@ -3,12 +3,15 @@ const chai = require('chai')
 const ExperimentsService = require('../../src/services/ExperimentsService')
 const db = require('../../src/db/DbManager')
 
-
+const tx = {}
 const getStub =sinon.stub(db.experiments,'all')
 const findStub =sinon.stub(db.experiments,'find')
 const updateStub = sinon.stub(db.experiments, 'update')
 const createStub = sinon.stub(db.experiments, 'create')
 const removeStub = sinon.stub(db.experiments, 'remove')
+const transactionStub = sinon.stub(db.experiments, 'repository', () => {
+    return {tx: function(transactionName, callback){return callback(tx)}}
+})
 
 after(() => {
     getStub.restore()
@@ -26,28 +29,28 @@ describe('ExperimentsService', () => {
         it('Success and Return 2 Experiments', (done)=> {
             getStub.resolves([
                 {
-                    "id": 30,
-                    "name": "exp1001",
-                    "subjectType": "plant",
-                    "reps": 20,
-                    "refExperimentDesignId": 2,
-                    "createdDate": "2016-10-05T15:19:12.026Z",
-                    "createdUserId": "akuma11",
-                    "modifiedUserId": "akuma11",
-                    "modifiedDate": "2016-10-05T15:19:12.026Z",
-                    "status": "ACTIVE"
+                    'id': 30,
+                    'name': 'exp1001',
+                    'subjectType': 'plant',
+                    'reps': 20,
+                    'refExperimentDesignId': 2,
+                    'createdDate': '2016-10-05T15:19:12.026Z',
+                    'createdUserId': 'akuma11',
+                    'modifiedUserId': 'akuma11',
+                    'modifiedDate': '2016-10-05T15:19:12.026Z',
+                    'status': 'ACTIVE'
                 },
                 {
-                    "id": 35,
-                    "name": "exp1001",
-                    "subjectType": "plant",
-                    "reps": 20,
-                    "refExperimentDesignId": 2,
-                    "createdDate": "2016-10-05T15:49:45.700Z",
-                    "createdUserId": "akuma11",
-                    "modifiedUserId": "akuma11",
-                    "modifiedDate": "2016-10-05T15:49:45.700Z",
-                    "status": "ACTIVE"
+                    'id': 35,
+                    'name': 'exp1001',
+                    'subjectType': 'plant',
+                    'reps': 20,
+                    'refExperimentDesignId': 2,
+                    'createdDate': '2016-10-05T15:49:45.700Z',
+                    'createdUserId': 'akuma11',
+                    'modifiedUserId': 'akuma11',
+                    'modifiedDate': '2016-10-05T15:49:45.700Z',
+                    'status': 'ACTIVE'
                 }
             ])
             const testObject = new ExperimentsService()
@@ -72,22 +75,22 @@ describe('ExperimentsService', () => {
         it('Success and Return experiment with Id', (done)=> {
             findStub.resolves(
                 {
-                    "id": 30,
-                    "name": "exp1001",
-                    "subjectType": "plant",
-                    "reps": 20,
-                    "refExperimentDesignId": 2,
-                    "createdDate": "2016-10-05T15:19:12.026Z",
-                    "createdUserId": "akuma11",
-                    "modifiedUserId": "akuma11",
-                    "modifiedDate": "2016-10-05T15:19:12.026Z",
-                    "status": "ACTIVE"
+                    'id': 30,
+                    'name': 'exp1001',
+                    'subjectType': 'plant',
+                    'reps': 20,
+                    'refExperimentDesignId': 2,
+                    'createdDate': '2016-10-05T15:19:12.026Z',
+                    'createdUserId': 'akuma11',
+                    'modifiedUserId': 'akuma11',
+                    'modifiedDate': '2016-10-05T15:19:12.026Z',
+                    'status': 'ACTIVE'
                 },
               )
             const testObject = new ExperimentsService()
             testObject.getExperimentById(30).then((experiment)=> {
                 experiment.id.should.equal(30)
-                experiment.name.should.equal("exp1001")
+                experiment.name.should.equal('exp1001')
             }).then(done,done)
         })
 
@@ -103,7 +106,7 @@ describe('ExperimentsService', () => {
             testObject.getExperimentById(30).should.be.rejected
             testObject.getExperimentById(30).catch((err) => {
                 err.validationMessages.length.should.equal(1)
-                err.validationMessages[0].should.equal("Experiment Not Found for requested experimentId")
+                err.validationMessages[0].should.equal('Experiment Not Found for requested experimentId')
             }).then(done, done)
 
         })
@@ -114,22 +117,22 @@ describe('ExperimentsService', () => {
     describe('create Experiments', () => {
 
         const experimentsObj=[{
-            "name": "exp1002",
-            "subjectType": "plant",
-            "reps": 20,
-            "refExperimentDesignId": 2,
-            "createdDate": "2016-10-05T15:19:12.026Z",
-            "createdUserId": "akuma11",
-            "modifiedUserId": "akuma11",
-            "modifiedDate": "2016-10-05T15:19:12.026Z",
-            "status": "ACTIVE"
+            'name': 'exp1002',
+            'subjectType': 'plant',
+            'reps': 20,
+            'refExperimentDesignId': 2,
+            'createdDate': '2016-10-05T15:19:12.026Z',
+            'createdUserId': 'akuma11',
+            'modifiedUserId': 'akuma11',
+            'modifiedDate': '2016-10-05T15:19:12.026Z',
+            'status': 'ACTIVE'
         }]
 
         const expectedResult= [
             {
-                "status":201,
-                "message":'Resource created',
-                "id":1
+                'status':201,
+                'message':'Resource created',
+                'id':1
             }
         ]
 
@@ -145,22 +148,22 @@ describe('ExperimentsService', () => {
         it('succeeds and returns list of experiment ids with status and message for batch experiment create request', (done) => {
             experimentsObj.push(
                 {
-                    "name": "exp1003",
-                    "subjectType": "plant",
-                    "reps": 20,
-                    "refExperimentDesignId": 2,
-                    "createdDate": "2016-10-05T15:19:12.026Z",
-                    "createdUserId": "akuma11",
-                    "modifiedUserId": "akuma11",
-                    "modifiedDate": "2016-10-05T15:19:12.026Z",
-                    "status": "ACTIVE"
+                    'name': 'exp1003',
+                    'subjectType': 'plant',
+                    'reps': 20,
+                    'refExperimentDesignId': 2,
+                    'createdDate': '2016-10-05T15:19:12.026Z',
+                    'createdUserId': 'akuma11',
+                    'modifiedUserId': 'akuma11',
+                    'modifiedDate': '2016-10-05T15:19:12.026Z',
+                    'status': 'ACTIVE'
                 }
             )
             expectedResult.push(
                     {
-                        "status":201,
-                        "message":'Resource created',
-                        "id":1
+                        'status':201,
+                        'message':'Resource created',
+                        'id':1
                     }
             )
 
@@ -184,16 +187,16 @@ describe('ExperimentsService', () => {
 
         it('Success and Return experiment', (done)=> {
             const experimentObj={
-                "id": 30,
-                "name": "exp1002",
-                "subjectType": "plant",
-                "reps": 20,
-                "refExperimentDesignId": 2,
-                "createdDate": "2016-10-05T15:19:12.026Z",
-                "createdUserId": "akuma11",
-                "modifiedUserId": "akuma11",
-                "modifiedDate": "2016-10-05T15:19:12.026Z",
-                "status": "ACTIVE"
+                'id': 30,
+                'name': 'exp1002',
+                'subjectType': 'plant',
+                'reps': 20,
+                'refExperimentDesignId': 2,
+                'createdDate': '2016-10-05T15:19:12.026Z',
+                'createdUserId': 'akuma11',
+                'modifiedUserId': 'akuma11',
+                'modifiedDate': '2016-10-05T15:19:12.026Z',
+                'status': 'ACTIVE'
             }
             updateStub.resolves(
                 experimentObj
@@ -201,22 +204,22 @@ describe('ExperimentsService', () => {
             const testObject = new ExperimentsService()
             testObject.updateExperiment(30,experimentObj).then((experiment)=> {
                 experiment.id.should.equal(30)
-                experiment.name.should.equal("exp1002")
+                experiment.name.should.equal('exp1002')
             }).then(done,done)
         })
 
         it('fails', (done) => {
             const experimentObj={
-                "id": 30,
-                "name": "exp1002",
-                "subjectType": "plant",
-                "reps": 20,
-                "refExperimentDesignId": 2,
-                "createdDate": "2016-10-05T15:19:12.026Z",
-                "createdUserId": "akuma11",
-                "modifiedUserId": "akuma11",
-                "modifiedDate": "2016-10-05T15:19:12.026Z",
-                "status": "ACTIVE"
+                'id': 30,
+                'name': 'exp1002',
+                'subjectType': 'plant',
+                'reps': 20,
+                'refExperimentDesignId': 2,
+                'createdDate': '2016-10-05T15:19:12.026Z',
+                'createdUserId': 'akuma11',
+                'modifiedUserId': 'akuma11',
+                'modifiedDate': '2016-10-05T15:19:12.026Z',
+                'status': 'ACTIVE'
             }
             updateStub.rejects({'status': 500, 'code': 'Internal Server Error', 'errorMessage': 'Please Contact Support'})
             const testObject = new ExperimentsService()
@@ -229,7 +232,7 @@ describe('ExperimentsService', () => {
             testObject.updateExperiment(30).should.be.rejected
             testObject.updateExperiment(30).catch((err) => {
                 err.validationMessages.length.should.equal(1)
-                err.validationMessages[0].should.equal("Experiment Not Found to Update")
+                err.validationMessages[0].should.equal('Experiment Not Found to Update')
             }).then(done, done)
 
         })
@@ -260,7 +263,7 @@ describe('ExperimentsService', () => {
             testObject.deleteExperiment(30).should.be.rejected
             testObject.deleteExperiment(30).catch((err) => {
                 err.validationMessages.length.should.equal(1)
-                err.validationMessages[0].should.equal("Experiment Not Found for requested experimentId")
+                err.validationMessages[0].should.equal('Experiment Not Found for requested experimentId')
             }).then(done, done)
 
         })

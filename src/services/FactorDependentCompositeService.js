e631d4996b6a7efbc445d4579d7f6f2ff8a4d40e
kmccl <kyle.mcclendon@monsanto.com>
2016-11-02 14:02:21 -0500

    Pushing Composite service get changes and specs

--- src/services/FactorDependentCompositeService.js
+++ src/services/FactorDependentCompositeService.js
@@ -1,10 +1,4 @@
-import db from "../db/DbManager"
-import AppUtil from "./utility/AppUtil"
-import AppError from "./utility/AppError"
-// import DependentVariablesValidator from "../validations/DependentVariablesValidator"
 import * as _ from 'lodash'
-
-import ExperimentsService from './ExperimentsService'
 import FactorLevelService from "./FactorLevelService"
 import FactorService from "./factorService"
 import DependentVariableService from "./DependentVariableService"
@@ -16,47 +10,61 @@ const logger = log4js.getLogger('FactorDependentCompositeService')
 class FactorDependentCompositeService {
 
     constructor() {
-        // this._validator = new DependentVariablesValidator()
-        this._experimentService = new ExperimentsService()
         this._factorLevelService = new FactorLevelService()
         this._factorService = new FactorService()
         this._dependentVariableService = new DependentVariableService()
         this._factorTypeService = new FactorTypeService()
     }
 
+    _getFactorsWithLevels(experimentId){
+        return this._getFactors(experimentId).then((factors) => {
+            return this._getFactorLevels(factors).then((levels) => {
+                return {factors: _.flatten(factors), levels: _.flatten(levels)}
+            })
+        })
+    }
+
+    _getFactors(experimentId){
+        return this._factorService.getFactorsByExperimentId(experimentId)
+    }
+
+    _getFactorLevels(factors){
+        return Promise.all(_.map(factors, (factor) => { return this._factorLevelService.getFactorLevelsByFactorId(factor.id)}))
+    }
+
     getAllVariablesByExperimentId(experimentId){
         return Promise.all(
             [
-                this._factorService.getFactorsByExperimentId(experimentId).then((values) => {
-                    return Promise.all(_.map(values, (value) => { return this._factorLevelService.getFactorLevelsByFactorId(value.id)})).then((levels) => {
-                        return [values, levels]
-                    })
-                }),
-                this._dependentVariableService.getDependentVariablesByExperimentId(experimentId),
-                this._factorTypeService.getAllFactorTypes()
+                this._getFactorsWithLevels(experimentId),
+                this._factorTypeService.getAllFactorTypes(),
+                this._dependentVariableService.getDependentVariablesByExperimentId(experimentId)
             ]
         ).then((value) => {
-            const object = {
+            const variablesObject = {
                 independent: [],
                 exogenous: [],
                 dependent: []
             }
 
-            const variables = _.map(value[0][0], (variable, index)=>{
-                const levels = _.map(value[0][1][index], (level) => { return level.value})
-                const type = _.find(value[2], {id: variable.ref_factor_type_id}).type.toLowerCase()
-                return {name: variable.name, type: type, levels: levels}
+            const variables = _.map(value[0].factors, (variable)=>{
+                const levels = _.filter(value[0].levels, (level) => { return level.factor_id == variable.id})
+                const levelValues = _.map(levels, (level) => { return level.value})
+
+                const type = _.find(value[1], {id: variable.ref_factor_type_id}).type.toLowerCase()
+                return {name: variable.name, type: type, levels: levelValues}
             })
 
             _.each(variables, (variable) => {
-                object[variable.type].push(variable)
+                const type = variable.type
+                delete variable['type']
+                variablesObject[type].push(variable)
             })
 
-            object.dependent = _.map(value[1], (dependentVariable) => {
+            variablesObject.dependent = _.map(value[2], (dependentVariable) => {
                 return {name: dependentVariable.name, required: dependentVariable.required}
             })
 
-            return object
+            return variablesObject
         })
     }
 }
---
+++ test/services/FactorDependentCompositeServiceSpec.js
@@ -0,0 +1,396 @@
+const sinon = require('sinon')
+const chai = require('chai')
+const FDCS = require('../../src/services/FactorDependentCompositeService')
+
+const testPayload = {}
+const testResponse = {}
+const testError = {}
+
+let getfactorStub
+let getfactorLevelStub
+let getdependentVariableStub
+let getfactorTypeStub
+
+let factorService, factorLevelService, dependentVariableService, factorTypeService
+
+let fdcs
+
+describe('FactorDependentCompositeService', () => {
+    before(() => {
+        fdcs = new FDCS()
+
+        factorService = fdcs._factorService
+        factorLevelService = fdcs._factorLevelService
+        dependentVariableService = fdcs._dependentVariableService
+        factorTypeService = fdcs._factorTypeService
+
+        getfactorStub = sinon.stub(factorService, 'getFactorsByExperimentId')
+        getfactorLevelStub = sinon.stub(factorLevelService, 'getFactorLevelsByFactorId')
+        getdependentVariableStub = sinon.stub(dependentVariableService, 'getDependentVariablesByExperimentId')
+        getfactorTypeStub = sinon.stub(factorTypeService, 'getAllFactorTypes')
+    })
+
+    after(() => {
+        getfactorStub.restore()
+        getfactorLevelStub.restore()
+        getdependentVariableStub.restore()
+        getfactorTypeStub.restore()
+    })
+
+    afterEach(() => {
+        getfactorStub.reset()
+        getfactorLevelStub.reset()
+        getdependentVariableStub.reset()
+        getfactorTypeStub.reset()
+    })
+
+    describe('getAllVariablesByExperimentId', function () {
+        it('successfully gets no factors and no levels', function () {
+            const getFactorWithLevelsStub = sinon.stub(fdcs, '_getFactorsWithLevels').resolves({
+                factors: [],
+                levels: []
+            })
+            getfactorTypeStub.resolves([])
+            getdependentVariableStub.resolves([])
+
+            return fdcs.getAllVariablesByExperimentId(1).then((data) => {
+                data.should.deep.equal({
+                    independent: [],
+                    exogenous: [],
+                    dependent: []
+                })
+                getFactorWithLevelsStub.calledWith(1).should.equal(true)
+                getfactorTypeStub.calledOnce.should.equal(true)
+                getdependentVariableStub.calledOnce.should.equal(true)
+
+                getFactorWithLevelsStub.restore()
+            })
+        })
+
+        it('successfully gets independent factors with levels', function () {
+            const getFactorWithLevelsStub = sinon.stub(fdcs, '_getFactorsWithLevels').resolves({
+                factors: [
+                    {id: 1, ref_factor_type_id: 1, name: 'TestFactor'},
+                    {id: 2, ref_factor_type_id: 1, name: 'TestFactor2'}
+                ],
+                levels: [
+                    {id: 1, factor_id: 1, value: "value1"},
+                    {id: 2, factor_id: 1, value: "value2"},
+                    {id: 3, factor_id: 2, value: "value3"}
+                ]
+            })
+            getfactorTypeStub.resolves([{id: 1, type: 'Independent'}])
+            getdependentVariableStub.resolves([])
+
+            return fdcs.getAllVariablesByExperimentId(1).then((data) => {
+                data.should.deep.equal({
+                    independent: [
+                        {
+                            name: 'TestFactor',
+                            levels: ['value1', 'value2']
+                        },
+                        {
+                            name: 'TestFactor2',
+                            levels: ['value3']
+                        }
+                    ],
+                    exogenous: [],
+                    dependent: []
+                })
+                getFactorWithLevelsStub.calledWith(1).should.equal(true)
+                getfactorTypeStub.calledOnce.should.equal(true)
+                getdependentVariableStub.calledOnce.should.equal(true)
+
+                getFactorWithLevelsStub.restore()
+            })
+        })
+
+        it('successfully gets exogenous factors with levels', function () {
+            const getFactorWithLevelsStub = sinon.stub(fdcs, '_getFactorsWithLevels').resolves({
+                factors: [
+                    {id: 1, ref_factor_type_id: 1, name: 'TestFactor'},
+                    {id: 2, ref_factor_type_id: 1, name: 'TestFactor2'}
+                ],
+                levels: [
+                    {id: 1, factor_id: 1, value: "value1"},
+                    {id: 2, factor_id: 1, value: "value2"},
+                    {id: 3, factor_id: 2, value: "value3"}
+                ]
+            })
+            getfactorTypeStub.resolves([{id: 1, type: 'Exogenous'}])
+            getdependentVariableStub.resolves([])
+
+            return fdcs.getAllVariablesByExperimentId(1).then((data) => {
+                data.should.deep.equal({
+                    independent: [],
+                    exogenous: [
+                        {
+                            name: 'TestFactor',
+                            levels: ['value1', 'value2']
+                        },
+                        {
+                            name: 'TestFactor2',
+                            levels: ['value3']
+                        }
+                    ],
+                    dependent: []
+                })
+                getFactorWithLevelsStub.calledWith(1).should.equal(true)
+                getfactorTypeStub.calledOnce.should.equal(true)
+                getdependentVariableStub.calledOnce.should.equal(true)
+
+                getFactorWithLevelsStub.restore()
+            })
+        })
+
+        it('successfully gets dependent variables', function () {
+            const getFactorWithLevelsStub = sinon.stub(fdcs, '_getFactorsWithLevels').resolves({
+                factors: [],
+                levels: []
+            })
+            getfactorTypeStub.resolves([{id: 1, type: 'Exogenous'}])
+            getdependentVariableStub.resolves([
+                {
+                    name: 'dependent1',
+                    required: true
+                },
+                {
+                    name: 'dependent2',
+                    required: false
+                }
+            ])
+
+            return fdcs.getAllVariablesByExperimentId(1).then((data) => {
+                data.should.deep.equal({
+                    independent: [],
+                    exogenous: [],
+                    dependent: [
+                        {
+                            name: 'dependent1',
+                            required: true
+                        },
+                        {
+                            name: 'dependent2',
+                            required: false
+                        }
+                    ]
+                })
+                getFactorWithLevelsStub.calledWith(1).should.equal(true)
+                getfactorTypeStub.calledOnce.should.equal(true)
+                getdependentVariableStub.calledOnce.should.equal(true)
+
+                getFactorWithLevelsStub.restore()
+            })
+        })
+
+        it('successfully gets all factors with levels', function () {
+            const getFactorWithLevelsStub = sinon.stub(fdcs, '_getFactorsWithLevels').resolves({
+                factors: [
+                    {id: 1, ref_factor_type_id: 1, name: 'TestFactor'},
+                    {id: 2, ref_factor_type_id: 1, name: 'TestFactor2'},
+                    {id: 3, ref_factor_type_id: 2, name: 'TestExogenous'}
+                ],
+                levels: [
+                    {id: 1, factor_id: 1, value: "value1"},
+                    {id: 2, factor_id: 1, value: "value2"},
+                    {id: 3, factor_id: 2, value: "value3"},
+                    {id: 4, factor_id: 3, value: 'exo1'},
+                    {id: 5, factor_id: 3, value: 'exo2'}
+                ]
+            })
+            getfactorTypeStub.resolves([{id: 1, type: 'Independent'}, {id: 2, type: 'Exogenous'}])
+            getdependentVariableStub.resolves([
+                {
+                    name: 'dependent1',
+                    required: true
+                },
+                {
+                    name: 'dependent2',
+                    required: false
+                }
+            ])
+
+            return fdcs.getAllVariablesByExperimentId(1).then((data) => {
+                data.should.deep.equal({
+                    independent: [
+                        {
+                            name: 'TestFactor',
+                            levels: ['value1', 'value2']
+                        },
+                        {
+                            name: 'TestFactor2',
+                            levels: ['value3']
+                        }
+                    ],
+                    exogenous: [
+                        {
+                            name: 'TestExogenous',
+                            levels: ['exo1', 'exo2']
+                        }
+                    ],
+                    dependent: [
+                        {
+                            name: 'dependent1',
+                            required: true
+                        },
+                        {
+                            name: 'dependent2',
+                            required: false
+                        }
+                    ]
+                })
+                getFactorWithLevelsStub.calledWith(1).should.equal(true)
+                getfactorTypeStub.calledOnce.should.equal(true)
+                getdependentVariableStub.calledOnce.should.equal(true)
+
+                getFactorWithLevelsStub.restore()
+            })
+        })
+
+        it('fails to retrieve factors', function () {
+            const getFactorWithLevelsStub = sinon.stub(fdcs, '_getFactorsWithLevels').rejects("Failure")
+            getfactorTypeStub.resolves([])
+            getdependentVariableStub.resolves([])
+
+            return fdcs.getAllVariablesByExperimentId(1).should.be.rejected.then((err) => {
+                getfactorTypeStub.calledOnce.should.equal(true)
+                getdependentVariableStub.calledOnce.should.equal(true)
+                err.message.should.equal("Failure")
+                getFactorWithLevelsStub.restore()
+            })
+
+        })
+    })
+
+    describe('_getFactorsWithLevels', function(){
+        let getFactorStub, getFactorLevelStub
+
+        before(() => {
+            getFactorStub = sinon.stub(fdcs, '_getFactors')
+            getFactorLevelStub = sinon.stub(fdcs, '_getFactorLevels')
+        })
+
+        after(() => {
+            getFactorStub.restore()
+            getFactorLevelStub.restore()
+        })
+
+        afterEach(() => {
+            getFactorStub.reset()
+            getFactorLevelStub.reset()
+        })
+
+        it('returns an object with no factors and no levels', function(){
+            getFactorStub.resolves([])
+            getFactorLevelStub.resolves([])
+
+            return fdcs._getFactorsWithLevels(1).then((result)=>{
+                result.should.deep.equal({
+                    factors: [],
+                    levels: []
+                })
+            })
+        })
+
+        it('returns an object with factors but no levels', function(){
+            getFactorStub.resolves([{name: 'Factor1', id: 1}])
+            getFactorLevelStub.resolves([])
+
+            return fdcs._getFactorsWithLevels(1).then((result)=>{
+                result.should.deep.equal({
+                    factors: [{name: 'Factor1', id: 1}],
+                    levels: []
+                })
+            })
+        })
+
+        it('returns an object with factors and levels', function(){
+            getFactorStub.resolves([{name: 'Factor1', id: 1}, {name: 'Factor2', id:2}])
+            getFactorLevelStub.resolves([[{value: 'level1', factor_id: 1}, {value: 'level2', factor_id: 1}],[{value: 'level3', factor_id:2}]])
+
+            return fdcs._getFactorsWithLevels(1).then((result)=>{
+                result.should.deep.equal({
+                    factors: [{name: 'Factor1', id: 1}, {name: 'Factor2', id: 2}],
+                    levels: [{value: 'level1', factor_id: 1}, {value: 'level2', factor_id: 1}, {value: 'level3', factor_id:2}]
+                })
+            })
+        })
+
+        it('fails to get factors', function(){
+            getFactorStub.rejects("Failed")
+
+            return fdcs._getFactorsWithLevels(1).should.be.rejected.then((err)=>{
+                err.message.should.equal("Failed")
+                getFactorLevelStub.called.should.equal(false)
+            })
+        })
+
+        it('fails to get factor levels', function(){
+            getFactorStub.resolves([{name: 'Factor1', id:1}])
+            getFactorLevelStub.rejects("Failed")
+
+            return fdcs._getFactorsWithLevels(1).should.be.rejected.then((err)=>{
+                err.message.should.equal("Failed")
+            })
+        })
+    })
+
+    describe('_getFactors', function(){
+        it('calls factor service and returns no factors', function(){
+            getfactorStub.resolves([])
+
+            return fdcs._getFactors(1).then((data)=>{
+                data.should.deep.equal([])
+            })
+        })
+
+        it('calls factor service and returns factors', function(){
+            getfactorStub.resolves([{name: 'Factor1', id:1}])
+
+            return fdcs._getFactors(1).then((data)=>{
+                data.should.deep.equal([{name: 'Factor1', id:1}])
+            })
+        })
+
+        it('calls factor service and fails', function(){
+            getfactorStub.rejects("Failed")
+
+            return fdcs._getFactors(1).should.be.rejected.then((err)=>{
+                err.message.should.equal("Failed")
+            })
+        })
+    })
+
+    describe('_getFactorLevels', function(){
+        it('calls factor level service and returns no levels', function(){
+            getfactorLevelStub.resolves([])
+            const factors = [{name: 'Test1', id:1}]
+
+            return fdcs._getFactorLevels(factors).then((data)=> {
+                data.should.deep.equal([[]])
+                getfactorLevelStub.calledOnce.should.equal(true)
+            })
+        })
+
+        it('calls factor level service for two factors', function(){
+            getfactorLevelStub.onFirstCall().resolves([]).onSecondCall().resolves([])
+            const factors = [{name: 'Test1', id:1}, {name: 'Test2', id:2}]
+
+            return fdcs._getFactorLevels(factors).then((data)=> {
+                data.should.deep.equal([[],[]])
+                getfactorLevelStub.calledTwice.should.equal(true)
+            })
+        })
+
+        it('calls factor level service and fails', function(){
+            getfactorLevelStub.onFirstCall().resolves([]).onSecondCall().rejects("Failed")
+            const factors = [{name: 'Test1', id:1}, {name: 'Test2', id:2}]
+
+            return fdcs._getFactorLevels(factors).should.be.rejected.then((err)=> {
+                err.message.should.equal('Failed')
+                getfactorLevelStub.calledTwice.should.equal(true)
+            })
+        })
+    })
+
+})

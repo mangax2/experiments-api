9cd6af77bb9dd8b25bbf727c1ddbdfbe422f169b
kmccl <kyle.mcclendon@monsanto.com>
2016-11-07 16:11:07 -0600

    remove -temp from log4js app name

--- log4js-conf.js
+++ log4js-conf.js
@@ -2,11 +2,8 @@ const log4js = require('log4js')
 
 module.exports = () => {
     const logLevel = "DEBUG"
-    console.log("12361283712983712983701982730981273098127039817209837120893701927309127390127309127309812730912830912739081270")
-    console.log(process.env.VCAP_APPLICATION)
     const isRunningInCloudFoundry = () => { return process.env.VCAP_APPLICATION }
     if(isRunningInCloudFoundry()){
-        console.log("ALDFJALSDKJFALSDKJFSLADKJFASLKDFJSADLKFJSDLKFJSDALFJASD>KJFASLDKFJASDLKJFLSADKJFLASDKJFSADLJF")
 
         const vcapApplication = JSON.parse(process.env.VCAP_APPLICATION)
         const config = {
@@ -17,7 +14,7 @@ module.exports = () => {
                         type: 'pattern',
                         pattern: 'log_level=\"%p\"  app=\"%x{app}\" app_host=\"%x{host}\" instance=\"%x{instanceIndex}\" - %m%n',
                         tokens: {
-                            app : vcapApplication.application_name,
+                            app : vcapApplication.application_name.includes('-temp') ? vcapApplication.application_name.substring(0, vcapApplication.application_name.length - 5) : vcapApplication.application_name,
                             instanceIndex : process.env.CF_INSTANCE_INDEX,
                             deploymentEnvironment: process.env.DEPLOYMENT_ENV,
                             host: process.env.CF_INSTANCE_IP

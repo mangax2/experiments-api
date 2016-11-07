b20e6bca834a1e53344c69b68efb862bc7dfff10
kmccl <kyle.mcclendon@monsanto.com>
2016-11-07 14:54:52 -0600

    testing call log4js func

--- log4js-conf.js
+++ log4js-conf.js
@@ -1,14 +1,11 @@
-/*eslint no-process-env: "warn"*/
-/*eslint no-undef: "warn"*/
-/*eslint no-console: "warn"*/
-
 const log4js = require('log4js')
 
 module.exports = () => {
     const logLevel = "DEBUG"
     console.log("12361283712983712983701982730981273098127039817209837120893701927309127390127309127309812730912830912739081270")
+    console.log(process.env.VCAP_APPLICATION)
     const isRunningInCloudFoundry = () => { return process.env.VCAP_APPLICATION }
-    if(isRunningInCloudFoundry){
+    if(isRunningInCloudFoundry()){
         console.log("ALDFJALSDKJFALSDKJFSLADKJFASLKDFJSADLKFJSDLKFJSDALFJASD>KJFASLDKFJASDLKJFLSADKJFLASDKJFSADLJF")
 
         const vcapApplication = JSON.parse(process.env.VCAP_APPLICATION)

1959df88e7cc9a24adc183e869e4e7d8e6ab9c9a
akuma11 <ajay.2.kumar@monsanto.com>
2017-01-18 17:26:20 -0600

    db password hide

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ config.env = process.env.ENV || 'local'
 config.port = process.env.PORT || 3001
 config.node_env = process.env.NODE_ENV || 'local'
 config.postgres_password = process.env.POSTGRES_PASSWORD
-config.db_password = process.env.POSTGRES_PASSWORD
+config.db_password = process.env.DB_PASSWORD
 config.exit = function(){process.exit(1)}
 config.watchUncaughtException = process.on('uncaughtException', function(error) {
     logger.fatal(error)

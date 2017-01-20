d297adbb0f7dc39c95a73c673176963efe30b027
akuma11 <ajay.2.kumar@monsanto.com>
2017-01-20 12:30:56 -0600

    view update in sql script

--- db-scripts/ProdRelease012017.sql
+++ db-scripts/ProdRelease012017.sql
@@ -303,3 +303,48 @@ INSERT INTO public.ref_factor_type (type, created_user_id, created_date, modifie
 
 INSERT INTO public.ref_factor_type (type, created_user_id, created_date, modified_user_id, modified_date)
   VALUES ('Exogenous', 'KMCCL', current_timestamp, 'KMCCL', current_timestamp);
+
+
+
+
+CREATE OR REPLACE VIEW public.experiment_summary AS
+ WITH treatment_numbers AS (
+         SELECT e_1.id AS experiment_id,
+            count(*) AS number_of_treatments
+           FROM experiment e_1
+             JOIN treatment t_1 ON t_1.experiment_id = e_1.id
+          GROUP BY e_1.id
+        ), dependent_variable_numbers AS (
+         SELECT e_1.id AS experiment_id,
+            count(*) AS number_of_dependent_variables
+           FROM experiment e_1
+             JOIN dependent_variable dv_1 ON dv_1.experiment_id = e_1.id
+          GROUP BY e_1.id
+        ), factor_numbers AS (
+         SELECT e_1.id AS experiment_id,
+            count(*) AS number_of_factors
+           FROM experiment e_1
+             JOIN factor f_1 ON f_1.experiment_id = e_1.id
+          GROUP BY e_1.id
+        ), experimental_unit_numbers AS (
+         SELECT e_1.id AS experiment_id,
+            count(*) AS number_of_experimental_units
+           FROM experiment e_1
+             JOIN treatment t_1 ON t_1.experiment_id = e_1.id
+             JOIN unit u ON u.treatment_id = t_1.id
+          GROUP BY e_1.id
+        )
+ SELECT e.id,
+    e.name,
+    (COALESCE(dv.number_of_dependent_variables, 0::bigint) + COALESCE(f.number_of_factors, 0::bigint))::integer AS number_of_variables,
+    COALESCE(t.number_of_treatments, 0::bigint)::integer AS number_of_treatments,
+    COALESCE(eu.number_of_experimental_units, 0::bigint)::integer AS number_of_experimental_units
+   FROM experiment e
+     LEFT JOIN treatment_numbers t ON t.experiment_id = e.id
+     LEFT JOIN dependent_variable_numbers dv ON dv.experiment_id = e.id
+     LEFT JOIN factor_numbers f ON f.experiment_id = e.id
+     LEFT JOIN experimental_unit_numbers eu ON eu.experiment_id = e.id;
+
+ALTER TABLE public.experiment_summary
+  OWNER TO experiments_secure_user;
+GRANT ALL ON TABLE public.experiment_summary TO experiments_secure_user;

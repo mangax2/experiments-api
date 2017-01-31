fb297faefd86c354eb96ed71f818f119b4bcd5d4
kmccl <kyle.mcclendon@monsanto.com>
2017-01-31 17:13:32 -0600

    Added tests for new group-unit structure

--- src/services/GroupExperimentalUnitCompositeService.js
+++ src/services/GroupExperimentalUnitCompositeService.js
@@ -1,10 +1,10 @@
-import Transactional from "../decorators/transactional";
-import GroupService from "./GroupService";
-import GroupValueService from "./GroupValueService";
-import ExperimentalUnitService from "./ExperimentalUnitService";
-import _ from "lodash";
-import db from "../db/DbManager";
-import AppUtil from "./utility/AppUtil";
+import Transactional from "../decorators/transactional"
+import GroupService from "./GroupService"
+import GroupValueService from "./GroupValueService"
+import ExperimentalUnitService from "./ExperimentalUnitService"
+import _ from "lodash"
+import db from "../db/DbManager"
+import AppUtil from "./utility/AppUtil"
 
 class GroupExperimentalUnitCompositeService {
 
@@ -18,7 +18,7 @@ class GroupExperimentalUnitCompositeService {
     saveGroupAndUnitDetails(experimentId, groupAndUnitDetails, context, tx) {
         const error = this._validateGroups(groupAndUnitDetails)
         if(error){
-           return  Promise.reject(error)
+            return  Promise.reject(error)
         }
         return this._groupService.deleteGroupsForExperimentId(experimentId, tx).then(()=> {
             return this._recursiveBatchCreate(experimentId,groupAndUnitDetails, context,tx).then(()=>{
@@ -29,24 +29,28 @@ class GroupExperimentalUnitCompositeService {
 
     _recursiveBatchCreate(experimentId,groupAndUnitDetails, context, tx) {
         const groups = _.map(groupAndUnitDetails, (gU)=> {
-                        gU.experimentId = experimentId
+            gU.experimentId = experimentId
             return _.omit(gU, ['groupValues', 'units', 'childGroups'])
         })
 
         return this._groupService.batchCreateGroups(groups, context, tx).then((groupResp)=> {
-            const _retVal = this._getUnitsAndGroupValues(groupResp, groupAndUnitDetails)
-            const promises = []
-            if (_retVal.groupValues.length > 0) {
-                promises.push(this._groupValueService.batchCreateGroupValues(_retVal.groupValues, context, tx))
-            }
-            if (_retVal.units.length > 0) {
-                promises.push(this._createExperimentalUnits(_retVal.units, context, experimentId, tx))
-            }
-            if (_retVal.childGroups.length > 0) {
-                promises.push(this._recursiveBatchCreate(experimentId,_retVal.childGroups,context,tx))
-            }
-            return Promise.all(promises)
+            return this._createGroupValuesUnitsAndChildGroups(experimentId, groupResp,groupAndUnitDetails,context,tx)
         })
+    }
+
+    _createGroupValuesUnitsAndChildGroups(experimentId, groupResponse, groupAndUnitDetails, context,tx){
+        const _retVal = this._getUnitsAndGroupValues(groupResponse, groupAndUnitDetails)
+        const promises = []
+        if (_retVal.groupValues.length > 0) {
+            promises.push(this._groupValueService.batchCreateGroupValues(_retVal.groupValues, context, tx))
+        }
+        if (_retVal.units.length > 0) {
+            promises.push(this._createExperimentalUnits(experimentId, _retVal.units, context, tx))
+        }
+        if (_retVal.childGroups.length > 0) {
+            promises.push(this._recursiveBatchCreate(experimentId,_retVal.childGroups,context,tx))
+        }
+        return Promise.all(promises)
     }
 
     _getUnitsAndGroupValues(groupResp, groupAndUnitDetails) {
@@ -76,59 +80,57 @@ class GroupExperimentalUnitCompositeService {
         let error=undefined
         _.forEach(groups, (grp)=> {
             if(!error) {
-                const units = grp['units']?grp['units']:[]
-                const childGroups = grp['childGroups']?grp['childGroups']:[]
-                if (units.length > 0 && childGroups.length > 0) {
-                    error = "Only leaf childGroups should have units"
-                }
-                if (units.length == 0 && childGroups.length == 0) {
-                    error = "Each group should have at least one Unit or at least one ChildGroup"
-                }
-                if (childGroups.length > 0) {
-                    error = this._validateGroups(childGroups)
-                }
+                error = this._validateGroup(grp)
             }
         })
         return error
     }
 
-
-
-assignGroupIdToGroupValuesAndUnits(groupAndUnitDetails, groupIds)
-{
-    _.forEach(groupAndUnitDetails, (gU, index)=> {
-        _.forEach(gU["groupValues"], (gV)=> gV.groupId = groupIds[index])
-        _.forEach(gU["units"], (u)=> u.groupId = groupIds[index])
-        _.forEach(gU["childGroups"], (cg)=> cg.parentId = groupIds[index])
-    })
-    return groupAndUnitDetails
-}
-
-@Transactional("getGroupAndUnitDetails")
-getGroupAndUnitDetails(experimentId, tx)
-{
-    return this._groupService.getGroupsByExperimentId(experimentId, tx).then((groups)=> {
-        const groupIds = _.map(groups, "id")
-        if (groupIds.length > 0) {
-            return Promise.all([
-                this._groupValueService.batchGetGroupValuesByGroupIds(groupIds, tx),
-                this._experimentalUnitService.batchGetExperimentalUnitsByGroupIds(groupIds, tx)]
-            ).then((groupValuesAndUnits) => {
-                const groupValues = _.compact(_.flatMap(groupValuesAndUnits[0]))
-                const units = _.compact(_.flatMap(groupValuesAndUnits[1]))
-                return _.map(groups, (group) => {
-                    group["groupValues"] = _.filter(groupValues, (g) => g["group_id"] == group.id)
-                    group["units"] = _.filter(units, (u) => u["group_id"] == group.id)
-                    return group
-                })
-            })
-        } else {
-            return []
+    _validateGroup(group){
+        const units = group['units'] ? group['units'] : []
+        const childGroups = group['childGroups'] ? group['childGroups'] : []
+        if (units.length > 0 && childGroups.length > 0) {
+            return "Only leaf childGroups should have units"
         }
-    })
-}
+        if (units.length == 0 && childGroups.length == 0) {
+            return "Each group should have at least one Unit or at least one ChildGroup"
+        }
+        if (childGroups.length > 0) {
+            return this._validateGroups(childGroups)
+        }
+    }
 
+    assignGroupIdToGroupValuesAndUnits(groupAndUnitDetails, groupIds) {
+        _.forEach(groupAndUnitDetails, (gU, index)=> {
+            _.forEach(gU["groupValues"], (gV)=> gV.groupId = groupIds[index])
+            _.forEach(gU["units"], (u)=> u.groupId = groupIds[index])
+            _.forEach(gU["childGroups"], (cg)=> cg.parentId = groupIds[index])
+        })
+        return groupAndUnitDetails
+    }
 
+    @Transactional("getGroupAndUnitDetails")
+    getGroupAndUnitDetails(experimentId, tx) {
+        return this._groupService.getGroupsByExperimentId(experimentId, tx).then((groups)=> {
+            const groupIds = _.map(groups, "id")
+            if (groupIds.length > 0) {
+                return Promise.all([
+                    this._groupValueService.batchGetGroupValuesByGroupIds(groupIds, tx),
+                    this._experimentalUnitService.batchGetExperimentalUnitsByGroupIds(groupIds, tx)]
+                ).then((groupValuesAndUnits) => {
+                    const groupValues = _.compact(_.flatMap(groupValuesAndUnits[0]))
+                    const units = _.compact(_.flatMap(groupValuesAndUnits[1]))
+                    return _.map(groups, (group) => {
+                        group["groupValues"] = _.filter(groupValues, (g) => g["group_id"] == group.id)
+                        group["units"] = _.filter(units, (u) => u["group_id"] == group.id)
+                        return group
+                    })
+                })
+            } else {
+                return []
+            }
+        })
+    }
 }
 
 module.exports = GroupExperimentalUnitCompositeService

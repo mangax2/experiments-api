512142496b9fbd4beee3120168b7e47c050755b7
kmccl <kyle.mcclendon@monsanto.com>
2017-02-14 15:25:46 -0600

    added postgres databse changes to new ddl script

--- db-scripts/Experiments_2_DDL.sql
+++ db-scripts/Experiments_2_DDL.sql
@@ -2,21 +2,21 @@ ALTER TABLE public.factor
    ADD tier numeric CHECK(tier > 0);
 
 CREATE TABLE ref_group_type (
-id serial NOT NULL,
-type varchar NOT NULL,
-created_user_id character varying NOT NULL,
-created_date timestamp with time zone NOT NULL,
-modified_user_id character varying NOT NULL,
-modified_date timestamp with time zone NOT NULL,
-CONSTRAINT ref_group_type_pk PRIMARY KEY (id),
-CONSTRAINT ref_group_type_ak_1 UNIQUE(type)
+  id serial NOT NULL,
+  type varchar NOT NULL,
+  created_user_id character varying NOT NULL,
+  created_date timestamp with time zone NOT NULL,
+  modified_user_id character varying NOT NULL,
+  modified_date timestamp with time zone NOT NULL,
+  CONSTRAINT ref_group_type_pk PRIMARY KEY (id),
+  CONSTRAINT ref_group_type_ak_1 UNIQUE(type)
 );
 
 ALTER TABLE public.group ADD COLUMN ref_group_type_id integer;
 
 ALTER TABLE public.group ADD CONSTRAINT "Group_Ref_Group_Type"
- FOREIGN KEY (ref_group_type_id) REFERENCES public.ref_group_type (id) MATCH SIMPLE
-ON UPDATE NO ACTION ON DELETE NO ACTION;
+  FOREIGN KEY (ref_group_type_id) REFERENCES public.ref_group_type (id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 INSERT INTO public.ref_group_type (type, created_user_id, created_date, modified_user_id, modified_date) VALUES ('Location', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
 INSERT INTO public.ref_group_type (type, created_user_id, created_date, modified_user_id, modified_date) VALUES ('Block', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);

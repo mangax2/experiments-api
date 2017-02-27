5796b2d3d4b10b256738715daddd011fbd2e30a7
akuma11 <ajay.2.kumar@monsanto.com>
2017-02-27 14:12:16 -0600

    + dev db config

--- db-scripts/Experiments_2_DDL.sql
+++ db-scripts/Experiments_2_DDL.sql
@@ -26,3 +26,64 @@ INSERT INTO public.ref_group_type (type, created_user_id, created_date, modified
 INSERT INTO public.ref_group_type (type, created_user_id, created_date, modified_user_id, modified_date) VALUES ('Block', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
 INSERT INTO public.ref_group_type (type, created_user_id, created_date, modified_user_id, modified_date) VALUES ('Rep', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
 INSERT INTO public.ref_group_type (type, created_user_id, created_date, modified_user_id, modified_date) VALUES ('Split Plot', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
+
+
+
+
+
+
+CREATE TABLE ref_unit_type
+(
+  id serial NOT NULL,
+  name character varying NOT NULL,
+  created_user_id character varying NOT NULL,
+  created_date timestamp with time zone NOT NULL,
+  modified_user_id character varying NOT NULL,
+  modified_date timestamp with time zone NOT NULL,
+  CONSTRAINT ref_unit_type_pk PRIMARY KEY (id),
+  CONSTRAINT "ref_unit_type_ak_1" UNIQUE (name)
+);
+
+
+CREATE TABLE ref_unit_spec
+(
+  id serial NOT NULL,
+  name character varying NOT NULL,
+  uom_type character varying,
+  ref_unit_type_id integer NOT NULL,
+  created_user_id character varying NOT NULL,
+  created_date timestamp with time zone NOT NULL,
+  modified_user_id character varying NOT NULL,
+  modified_date timestamp with time zone NOT NULL,
+  CONSTRAINT ref_unit_spec_pk PRIMARY KEY (id),
+  CONSTRAINT "Ref_Unit_Spec_Ref_Unit_Type" FOREIGN KEY (ref_unit_type_id)
+      REFERENCES ref_unit_type (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "ref_unit_spec_ak_1" UNIQUE  (name, ref_unit_type_id)
+
+);
+
+
+CREATE TABLE unit_spec_detail
+(
+  id serial NOT NULL,
+  value character varying NOT NULL,
+  uom_id integer,
+  ref_unit_spec_id integer NOT NULL,
+  experiment_id integer NOT NULL,
+  created_user_id character varying NOT NULL,
+  created_date timestamp with time zone NOT NULL,
+  modified_user_id character varying NOT NULL,
+  modified_date timestamp with time zone NOT NULL,
+  CONSTRAINT unit_spec_detail_pk PRIMARY KEY (id),
+  CONSTRAINT "Unit_Spec_Detail_Experiment" FOREIGN KEY (experiment_id)
+      REFERENCES experiment (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT "Unit_Spec_Detail_Ref_Unit_Spec" FOREIGN KEY (ref_unit_spec_id)
+      REFERENCES ref_unit_spec (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "unit_spec_detail_ak_1"  UNIQUE (ref_unit_spec_id, experiment_id)
+
+);
+
+

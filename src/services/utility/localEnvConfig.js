bbb2df9265692c5b450b3c1fbebac7ff1284dc2d
kmccl <kyle.mcclendon@monsanto.com>
2017-02-27 13:24:30 -0600

    fixed port for dev

--- src/services/utility/localEnvConfig.js
+++ src/services/utility/localEnvConfig.js
@@ -5,7 +5,7 @@ const cfServices = {
     experimentsDataSource: {
         alias: 'experimentsDataSource',
         host: config.env === "dev" ? "velocity-experiments-db.c6ftfip45sqv.us-east-1.rds.amazonaws.com" : 'localhost',
-        port: config.env === "dev" ? "velocity-experiments-db.c6ftfip45sqv.us-east-1.rds.amazonaws.com" : '9000',
+        port: config.env === "dev" ? "5432" : '9000',
         user: process.env.EXPERIMENTS_DB_LOCAL_USER,
         password: process.env.EXPERIMENTS_DB_LOCAL_PASSWORD,
         database: 'draft',

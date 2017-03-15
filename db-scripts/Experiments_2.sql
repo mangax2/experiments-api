7fafa76d3e99113e2d58f219cbdc2b46441968de
kprat1 <kamaraju.prathi@monsanto.com>
2017-03-15 10:21:32 -0500

    VE 1866 - Script Changed for fk

--- db-scripts/Experiments_2.sql
+++ db-scripts/Experiments_2.sql
@@ -9,5 +9,8 @@ CREATE TABLE tag
   modified_user_id character varying NOT NULL,
   modified_date timestamp with time zone NOT NULL,
   CONSTRAINT tag_pk PRIMARY KEY (id),
+  CONSTRAINT experiment_id FOREIGN KEY (experiment_id)
+      REFERENCES public.experiment (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT tag_ak_1 UNIQUE (name, value, experiment_id)
 )

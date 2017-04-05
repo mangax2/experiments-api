a5116776e184cc422ad062e848cb796939646950
akuma11 <ajay.2.kumar@monsanto.com>
2017-04-05 17:01:01 -0500

    sql update

--- db-scripts/ProdRelease_next.sql
+++ db-scripts/ProdRelease_next.sql
@@ -8,7 +8,7 @@ CREATE TABLE ref_data_source_type
   modified_date timestamp with time zone NOT NULL,
   CONSTRAINT ref_data_source_type_pk PRIMARY KEY (id),
   CONSTRAINT ref_data_source_type_ak_1 UNIQUE (type)
-)
+);
 
 INSERT INTO public.ref_data_source_type(
              type, created_user_id, created_date, modified_user_id, modified_date)
@@ -31,7 +31,7 @@ CREATE TABLE ref_data_source
       REFERENCES public.ref_data_source_type (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT ref_data_source_ak_1 UNIQUE (name)
-)
+);
 -----Update FK based on Primary Key------------------
 INSERT INTO public.ref_data_source(
             name, ref_data_source_type_id, created_user_id, created_date,
@@ -46,12 +46,23 @@ INSERT INTO public.ref_data_source(
             'KPRAT1', CURRENT_TIMESTAMP);
 
 
-ALTER TABLE public.factor ADD ref_data_source_id integer
+ALTER TABLE public.factor ADD ref_data_source_id integer;
 ALTER TABLE public.factor ADD CONSTRAINT factor_data_source_fk FOREIGN KEY (ref_data_source_id) REFERENCES public.ref_data_source(id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 
----------------------Run after the table creation---------------------------------
+
+---------------------Run after the table creation dev---------------------------------
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO experiments_dev_app_user;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO experiments_dev_app_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO experiments_ro_user;
+
+---------------------Run after the table creation non prod---------------------------------
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO experiments_app_user;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO experiments_app_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO experiments_ro_user;
+
+---------------------Run after the table creation prod---------------------------------
 GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO experiments_secure_app_user;
 GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO experiments_secure_app_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO experiments_secure_ro_user;

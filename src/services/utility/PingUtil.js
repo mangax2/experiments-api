9b71852e4205ccfcbf1d33ff9320a1265be249e2
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2017-04-20 15:23:48 -0500

    Even more logging.

--- src/services/utility/PingUtil.js
+++ src/services/utility/PingUtil.js
@@ -1,5 +1,8 @@
+const log4js = require('log4js')
 const oauthPing = require('@monsantoit/oauth-ping')
 const cfServices = require('./ServiceConfig')
+
+const logger = log4js.getLogger('PingUtil')
 
 class PingUtil {
   static getMonsantoHeader() {
@@ -8,6 +11,9 @@ class PingUtil {
       clientSecret: cfServices.pingDataSource.clientSecret,
       url: cfServices.pingDataSource.url,
     }
+    logger.info(`client id: '${params.clientId}'`)
+    logger.info(`client secret: '${params.clientSecret}'`)
+    logger.info(`url: '${params.url}'`)
     return oauthPing.httpGetToken(params).then(token => [
       { headerName: 'authorization', headerValue: `Bearer ${token}` },
       { headerName: 'Content-Type', headerValue: 'application/json' },

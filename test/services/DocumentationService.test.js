f672ea320ad9c2db2d0b4a6015af54cfd3e54f5b
KMCCL <kyle.mcclendon@monsanto.com>
2017-04-29 17:11:47 -0500

    doc stuff 6

--- test/services/DocumentationService.test.js
+++ test/services/DocumentationService.test.js
@@ -8,20 +8,8 @@ import HttpUtil from '../../src/services/utility/HttpUtil'
 describe('DocumentationService', () => {
 
   describe('getCloudFrontCookies', () => {
-    it('returns cookies when they already exist and are not expired', () => {
-      DocumentationService.cookies = {}
-      DocumentationService.cookieExpire = 999999999999999
-      cf.getSignedCookies = mock()
-
-      expect(DocumentationService.getCloudfrontCookies()).toEqual({})
-      expect(cf.getSignedCookies).not.toHaveBeenCalled()
-    })
-
-    it('calls getSignedCookie when cookies are undefined, and sets cookieExpire to 0 when it is' +
+    it('calls getSignedCookie' +
       ' undefined', () => {
-      DocumentationService.cookies = undefined
-      DocumentationService.cookieExpire = undefined
-
       cf.getSignedCookies = mock({})
 
       const result = DocumentationService.getCloudfrontCookies()
@@ -31,8 +19,6 @@ describe('DocumentationService', () => {
     })
 
     it('calls getSignedCookie for prod', () => {
-      DocumentationService.cookies = undefined
-      DocumentationService.cookieExpire = 999999999999999
       config.env = 'prod'
       VaultUtil.cloudFrontKeyPair = ''
       VaultUtil.cloudFrontSecret = ''
@@ -60,7 +46,7 @@ describe('DocumentationService', () => {
     it('calls getCloudfrontCookies and HttpUtil get', () => {
       DocumentationService.getCloudfrontCookies = mock({header: 'value', header2: 'value2'})
       HttpUtil.get = mockResolve()
-      const headers = [{headerName: 'Accept', headerValue: 'text/plain'}, {headerName: 'Cookie', headerValue: 'header=value; header2=value2'}]
+      const headers = [{headerName: 'Accept', headerValue: 'text/markdown'}, {headerName: 'Cookie', headerValue: 'header=value; header2=value2'}]
 
       return DocumentationService.getDoc('topic', 'doc.md').then(() => {
         expect(HttpUtil.get).toHaveBeenCalledWith('http://dcb6g58iy3guq.cloudfront.net/topic/doc.md', headers)

b92dd5930c52b73ffe8a8421b42d680f163f18ab
KMCCL <kyle.mcclendon@monsanto.com>
2017-05-08 11:32:09 -0500

    fixed? prod issue

--- src/services/DocumentationService.js
+++ src/services/DocumentationService.js
@@ -44,21 +44,14 @@ class DocumentationService {
   static getCloudfrontCookies() {
     const keyPairId = config.env === 'local' ? 'APKAIDNVPE572RTKAYCQ' : VaultUtil.cloudFrontKeyPair
     const privateKeyPath = `./src/pk-${keyPairId}.pem`
-    // const privateKeyString = config.env === 'local' ? undefined : VaultUtil.cloudFrontSecret
-    const url = config.env === 'prod' ? '' : 'http://dcb6g58iy3guq.cloudfront.net/*'
+    const url = 'http://dcb6g58iy3guq.cloudfront.net/*'
 
     const options = {
       expireTime: new Date().getTime() + (4 * 60 * 60 * 1000),
       keypairId: keyPairId,
     }
 
-    // if (privateKeyPath) {
-    //   console.info('This should not be getting hit!')
     options.privateKeyPath = privateKeyPath
-    // } else {
-    //   options.privateKeyString = privateKeyString
-    //   options.privateKeyString.replace(/\\n/g, '\n')
-    // }
 
     return cf.getSignedCookies(url, options)
   }

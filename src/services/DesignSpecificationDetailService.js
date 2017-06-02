620ef05714182704892b40b721f9e6cbda20d179
akuma11 <ajay.2.kumar@monsanto.com>
2017-06-02 10:07:10 -0500

    minor changes

--- src/services/DesignSpecificationDetailService.js
+++ src/services/DesignSpecificationDetailService.js
@@ -39,7 +39,7 @@ class DesignSpecificationDetailService {
   batchGetDesignSpecificationDetailsByIds = (ids, tx) =>
     db.designSpecificationDetail.batchFind(ids, tx)
       .then((data) => {
-        if (_.filter(data, element => element !== null).length !== ids.length) {
+        if (data.length !== ids.length) {
           logger.error('Design Specification Detail not found for all requested ids.')
           throw AppError.notFound('Design Specification Detail not found for all requested ids.')
         } else {
@@ -63,7 +63,7 @@ class DesignSpecificationDetailService {
 
   populateExperimentId = (designSpecs, experimentId) => {
     _.forEach(designSpecs, (ds) => {
-      ds.experimentId = parseInt(experimentId, 10)
+      ds.experimentId = Number(experimentId)
     })
   }
 
@@ -90,7 +90,7 @@ class DesignSpecificationDetailService {
     }
     return db.designSpecificationDetail.batchRemove(idsToDelete, tx)
       .then((data) => {
-        if (_.filter(data, element => element !== null).length !== idsToDelete.length) {
+        if (data.length !== idsToDelete.length) {
           logger.error('Not all design specification detail ids requested for delete were' +
             ' found')
           throw AppError.notFound(

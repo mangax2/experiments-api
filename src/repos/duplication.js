c07a6e291280b9af2d89fe2ae5994f52090d11ee
KMCCL <kyle.mcclendon@monsanto.com>
2017-06-13 16:23:15 -0500

    changed identifiers in sql

--- src/repos/duplication.js
+++ src/repos/duplication.js
@@ -4,35 +4,35 @@ module.exports = (rep, pgp) => ({
   duplicateExperiment: (experimentId, context, tx = rep) => tx.oneOrNone(
     "WITH ins_parent AS (" +
     "INSERT INTO experiment " +
-    "SELECT (t1).* FROM (" +
-      "SELECT t " +
+    "SELECT (e1).* FROM (" +
+      "SELECT e " +
         "#= hstore('id', nextval(pg_get_serial_sequence('experiment', 'id'))::text) " +
         "#= hstore('created_date', CURRENT_TIMESTAMP::text) " +
         "#= hstore('modified_date', CURRENT_TIMESTAMP::text) " +
         "#= hstore('created_user_id', $2) " +
         "#= hstore('modified_user_id', $2) " +
-        "#= hstore('name', CAST(('COPY OF ' || t.name) AS varchar(100))) AS t1 " +
-      "FROM experiment t " +
+        "#= hstore('name', CAST(('COPY OF ' || e.name) AS varchar(100))) AS e1 " +
+      "FROM experiment e " +
       "WHERE id = $1) sub " +
       "RETURNING id" +
     "), owner_ids AS (" +
     "INSERT INTO owner " +
     "SELECT (c).* " +
       "FROM  (" +
-        "SELECT ct " +
+        "SELECT o " +
           "#= hstore('id', nextval(pg_get_serial_sequence('owner', 'id'))::text) " +
           "#= hstore('user_ids', " +
-          "CASE WHEN ct.user_ids @> ARRAY[$2]::varchar[] THEN " +
-          "ct.user_ids::text " +
+          "CASE WHEN o.user_ids @> ARRAY[$2]::varchar[] THEN " +
+          "o.user_ids::text " +
           "ELSE " +
-          "array_cat(ct.user_ids, ARRAY[$2]::varchar[])::text " +
+          "array_cat(o.user_ids, ARRAY[$2]::varchar[])::text " +
           "END) " +
           "#= hstore('created_date', CURRENT_TIMESTAMP::text) " +
           "#= hstore('modified_date', CURRENT_TIMESTAMP::text) " +
           "#= hstore('created_user_id', $2) " +
           "#= hstore('modified_user_id', $2) " +
           "#= hstore('experiment_id', (SELECT id::text FROM ins_parent)) AS c " +
-        "FROM owner ct " +
+        "FROM owner o " +
         "WHERE experiment_id = $1 ) sub " +
       "RETURNING id) " +
     "SELECT * FROM ins_parent;",

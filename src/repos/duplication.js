1ee9721f14530c441142f627f617bcf771d1c470
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2017-06-22 11:34:50 -0500

    Updated the duplication script to include units and groups.

--- src/repos/duplication.js
+++ src/repos/duplication.js
@@ -1,5 +1,5 @@
 const duplicateExperimentInfoScript =
-  "WITH experiment_parent AS (" +
+  "WITH temp_experiment_parent AS (" +
     "INSERT INTO experiment " +
     "SELECT (e1).* FROM (" +
       "SELECT e " +
@@ -12,10 +12,13 @@ const duplicateExperimentInfoScript =
       "AS e1 FROM experiment e " +
     "WHERE id = $1) sub " +
     "RETURNING id" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP experiment_parent " +
+  "FROM temp_experiment_parent;"
 
 const duplicateOwnersScript =
-  "owner_ids AS (" +
+  "WITH temp_owner_ids AS (" +
     "INSERT INTO owner " +
     "SELECT (c).* FROM  (" +
       "SELECT o " +
@@ -34,10 +37,13 @@ const duplicateOwnersScript =
       "AS c FROM owner o " +
     "WHERE experiment_id = $1 ) sub " +
     "RETURNING id" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP owner_ids " +
+  "FROM temp_owner_ids;"
 
 const duplicateDependentVariableScript =
-  "dependent_variable_ids AS (" +
+  "WITH temp_dependent_variable_ids AS (" +
     "INSERT INTO dependent_variable " +
     "SELECT (c).* FROM  (" +
       "SELECT dv " +
@@ -50,10 +56,13 @@ const duplicateDependentVariableScript =
       "AS c FROM dependent_variable dv " +
     "WHERE experiment_id = $1 ) sub " +
     "RETURNING id" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP dependent_variable_ids " +
+  "FROM temp_dependent_variable_ids;"
 
 const duplicateFactorScript =
-  "new_factors AS (" +
+  "WITH temp_new_factors AS (" +
     "INSERT INTO factor " +
       "SELECT (c).* FROM  (" +
         "SELECT f " +
@@ -66,15 +75,18 @@ const duplicateFactorScript =
         "AS c FROM factor f " +
     "WHERE experiment_id = $1 ) sub " +
     "RETURNING id, name" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP new_factors " +
+  "FROM temp_new_factors;"
 
 const duplicateFactorLevelScript =
-  "mapped_factor_ids AS (" +
-    "SELECT f.id AS old_id, n.id AS new_id " +
-    "FROM factor f " +
-      "INNER JOIN new_factors n ON f.name = n.name " +
-    "WHERE f.experiment_id = $1" +
-    "), factor_level_ids AS (" +
+  "SELECT f.id AS old_id, n.id AS new_id " +
+  "INTO TEMP mapped_factor_ids " +
+  "FROM factor f " +
+    "INNER JOIN new_factors n ON f.name = n.name " +
+  "WHERE f.experiment_id = $1" +
+  "; WITH temp_factor_level_ids AS (" +
     "INSERT INTO factor_level " +
     "SELECT (c).* FROM  (" +
       "SELECT fl " +
@@ -87,10 +99,13 @@ const duplicateFactorLevelScript =
       "FROM factor_level fl " +
         "INNER JOIN mapped_factor_ids mfi ON fl.factor_id = mfi.old_id ) sub " +
     "RETURNING id" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP factor_level_ids " +
+  "FROM temp_factor_level_ids;"
 
 const duplicateTreatmentScript =
-  "new_treatments AS (" +
+  "WITH temp_new_treatments AS (" +
       "INSERT INTO treatment " +
       "SELECT (c).* FROM (" +
         "SELECT t " +
@@ -103,15 +118,18 @@ const duplicateTreatmentScript =
         "AS c FROM treatment t " +
       "WHERE experiment_id = $1 ) sub " +
       "RETURNING id, treatment_number" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP new_treatments " +
+  "FROM temp_new_treatments;"
 
 const duplicateCombinationElementScript =
-  "mapped_treatment_ids AS (" +
-    "SELECT t.id AS old_id, n.id AS new_id " +
-    "FROM treatment t " +
-      "INNER JOIN new_treatments n ON t.treatment_number = n.treatment_number " +
-    "WHERE t.experiment_id = $1" +
-    "), combination_element_ids AS (" +
+  "SELECT t.id AS old_id, n.id AS new_id " +
+  "INTO TEMP mapped_treatment_ids " +
+  "FROM treatment t " +
+    "INNER JOIN new_treatments n ON t.treatment_number = n.treatment_number " +
+  "WHERE t.experiment_id = $1;" +
+  "WITH temp_combination_element_ids AS (" +
     "INSERT INTO combination_element " +
     "SELECT (c).* FROM (" +
       "SELECT ce " +
@@ -124,10 +142,13 @@ const duplicateCombinationElementScript =
       "FROM combination_element ce " +
         "INNER JOIN mapped_treatment_ids mti ON ce.treatment_id = mti.old_id ) sub " +
     "RETURNING id" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP combination_element_ids " +
+  "FROM temp_combination_element_ids;"
 
 const duplicateUnitSpecificationScript =
-  "unit_spec_detail_ids AS (" +
+  "WITH temp_unit_spec_detail_ids AS (" +
     "INSERT INTO unit_spec_detail " +
     "SELECT (c).* FROM  (" +
       "SELECT usd " +
@@ -140,10 +161,13 @@ const duplicateUnitSpecificationScript =
       "AS c FROM unit_spec_detail usd " +
     "WHERE experiment_id = $1 ) sub " +
     "RETURNING id" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP unit_spec_detail_ids " +
+  "FROM temp_unit_spec_detail_ids;"
 
 const duplicateDesignSpecificationScript =
-  "design_spec_detail_ids AS (" +
+  "WITH temp_design_spec_detail_ids AS (" +
     "INSERT INTO design_spec_detail " +
     "SELECT (c).* FROM  (" +
       "SELECT dsd " +
@@ -156,23 +180,95 @@ const duplicateDesignSpecificationScript =
       "AS c FROM design_spec_detail dsd " +
     "WHERE experiment_id = $1 ) sub " +
     "RETURNING id" +
-  ")"
+  ")" +
+  "SELECT * " +
+  "INTO TEMP design_spec_detail_ids " +
+  "FROM temp_design_spec_detail_ids;"
+
+const duplicateGroupScript = 
+  "WITH temp_mapped_group_ids AS (" +
+    "INSERT INTO public.group " +
+    "SELECT (c).* FROM (" +
+      "SELECT g " +
+        "#= hstore('id', nextval(pg_get_serial_sequence('group', 'id'))::text) " +
+        "#= hstore('created_date', CURRENT_TIMESTAMP::text) " +
+        "#= hstore('modified_date', CURRENT_TIMESTAMP::text) " +
+        "#= hstore('created_user_id', $2) " +
+        "#= hstore('modified_user_id', $2) " +
+        "#= hstore('parent_id', g.id::text) " +
+        "#= hstore('experiment_id', (SELECT id::text FROM experiment_parent)) " +
+      "AS c FROM public.group g " +
+    "WHERE experiment_id = $1) sub " +
+    "RETURNING id AS new_id, parent_id AS old_id" +
+  ")" +
+  "SELECT * " +
+  "INTO TEMP mapped_group_ids " +
+  "FROM temp_mapped_group_ids;" +
+  "UPDATE public.group " +
+  "SET parent_id = mgi.new_id " +
+  "FROM public.group gtc " +
+    "INNER JOIN mapped_group_ids mgi ON gtc.parent_id = mgi.old_id " +
+  "WHERE public.group.id IN (SELECT new_id FROM mapped_group_ids) " +
+    "AND public.group.parent_id = gtc.id;"
+
+const duplicateGroupValueScript =
+  "WITH temp_group_value_ids AS (" +
+    "INSERT INTO group_value " +
+    "SELECT (c).* FROM (" +
+      "SELECT gv " +
+        "#= hstore('id', nextval(pg_get_serial_sequence('group_value', 'id'))::text) " +
+        "#= hstore('created_date', CURRENT_TIMESTAMP::text) " +
+        "#= hstore('modified_date', CURRENT_TIMESTAMP::text) " +
+        "#= hstore('created_user_id', $2) " +
+        "#= hstore('modified_user_id', $2) " +
+        "#= hstore('group_id', mgi.new_id::text) " +
+      "AS c FROM group_value gv " +
+        "INNER JOIN mapped_group_ids mgi ON gv.group_id = mgi.old_id) sub " +
+    "RETURNING id" +
+  ")" +
+  "SELECT * " +
+  "INTO TEMP group_value_ids " +
+  "FROM temp_group_value_ids;"
+
+const duplicateUnitScript =
+  "WITH temp_unit_ids AS (" +
+    "INSERT INTO unit " +
+    "SELECT (c).* FROM (" +
+      "SELECT u " +
+        "#= hstore('id', nextval(pg_get_serial_sequence('unit', 'id'))::text) " +
+        "#= hstore('created_date', CURRENT_TIMESTAMP::text) " +
+        "#= hstore('modified_date', CURRENT_TIMESTAMP::text) " +
+        "#= hstore('created_user_id', $2) " +
+        "#= hstore('modified_user_id', $2) " +
+        "#= hstore('group_id', mgi.new_id::text) " +
+        "#= hstore('treatment_id', mti.new_id::text) " +
+      "AS c FROM unit u " +
+        "INNER JOIN mapped_treatment_ids mti ON u.treatment_id = mti.old_id " +
+        "INNER JOIN mapped_group_ids mgi ON u.group_id = mgi.old_id) sub " +
+    "RETURNING id" +
+  ")" +
+  "SELECT * " +
+  "INTO TEMP unit_ids " +
+  "FROM temp_unit_ids;"
 
 
 module.exports = (rep, pgp) => ({
   repository: () => rep,
 
   duplicateExperiment: function(experimentId, context, tx = rep){ return tx.oneOrNone(
-    duplicateExperimentInfoScript +
-    ", " + duplicateOwnersScript +
-    ", " + duplicateFactorScript +
-    ", " + duplicateFactorLevelScript +
-    ", " + duplicateDependentVariableScript +
-    ", " + duplicateTreatmentScript +
-    ", " + duplicateCombinationElementScript +
-    ", " + duplicateUnitSpecificationScript +
-    ", " + duplicateDesignSpecificationScript +
-    " SELECT * FROM experiment_parent;",
+      duplicateExperimentInfoScript +
+        duplicateOwnersScript +
+        duplicateFactorScript +
+        duplicateFactorLevelScript +
+        duplicateDependentVariableScript +
+        duplicateTreatmentScript +
+        duplicateCombinationElementScript +
+        duplicateUnitSpecificationScript +
+        duplicateDesignSpecificationScript +
+        duplicateGroupScript +
+        duplicateGroupValueScript +
+        duplicateUnitScript +
+      " SELECT * FROM experiment_parent;",
     [experimentId, context.userId],
   )
   },

a278632addbe57e4140f9c1775d0cb7f314a64b1
KMCCL <kyle.mcclendon@monsanto.com>
2017-06-27 15:36:22 -0500

    moved prod release sql from next to when it was released

---
+++ db-scripts/ProdRelease060192017.sql
@@ -0,0 +1,16 @@
+ALTER TABLE public.owner ADD COLUMN group_ids character varying ARRAY;
+drop table tag;
+
+UPDATE public.owner
+SET group_ids = Array[]::varchar[]
+WHERE group_ids IS NULL;
+
+UPDATE public.owner
+SET user_ids = Array[]::varchar[]
+WHERE user_ids IS NULL;
+
+ALTER TABLE public.owner
+  ALTER COLUMN user_ids SET NOT NULL,
+  ALTER COLUMN group_ids SET NOT NULL,
+  ALTER COLUMN user_ids SET DEFAULT Array[]::varchar[],
+  ALTER COLUMN group_ids SET DEFAULT Array[]::varchar[];

1a60c5db66fd49764ad1c09a16cec8d9e3b35001
pnwatt <pnwatt@monsanto.com>
2017-08-25 11:52:44 -0500

    Converted factor level batch inserts and updates to use helpers.

--- src/repos/factorLevel.js
+++ src/repos/factorLevel.js
@@ -12,25 +12,52 @@ module.exports = (rep, pgp) => ({
 
   all: () => rep.any('SELECT ${columns} FROM factor_level_new'),
 
-  batchCreate: (factorLevels, context, tx = rep) => tx.batch(
-    factorLevels.map(
-      factorLevel => tx.one(
-        'INSERT INTO factor_level_new(id, value, factor_id, created_user_id, created_date, modified_user_id, modified_date) ' +
-        'VALUES(nextval(pg_get_serial_sequence(\'factor_level\', \'id\')), $1:json, $2, $3, CURRENT_TIMESTAMP, $3, CURRENT_TIMESTAMP) RETURNING id',
-        [factorLevel.value, factorLevel.factorId, context.userId],
-      ),
-    ),
-  ),
+  batchCreate: (factorLevels, context, tx = rep) => {
+    const columnSet = new pgp.helpers.ColumnSet(
+      [
+        'id:raw',
+        'value:raw',
+        'factor_id',
+        'created_user_id',
+        'created_date:raw',
+        'modified_user_id',
+        'modified_date:raw'
+      ],
+      {table: 'factor_level_new'})
+    const values = factorLevels.map(factorLevel => ({
+      id: 'nextval(pg_get_serial_sequence(\'factor_level\', \'id\'))',
+      value: `CAST(${pgp.as.json(factorLevel.value)} AS jsonb)`,
+      factor_id: factorLevel.factorId,
+      created_user_id: context.userId,
+      created_date: 'CURRENT_TIMESTAMP',
+      modified_user_id: context.userId,
+      modified_date: 'CURRENT_TIMESTAMP'
+    }))
+    const query = `${pgp.helpers.insert(values, columnSet)} RETURNING id`
+    return tx.any(query)
+  },
 
-  batchUpdate: (factorLevels, context, tx = rep) => tx.batch(
-    factorLevels.map(
-      factorLevel => tx.oneOrNone(
-        'UPDATE factor_level_new SET (value, factor_id, modified_user_id, modified_date) = ' +
-        '($1:json, $2, $3, CURRENT_TIMESTAMP) WHERE id = $4 RETURNING *',
-        [factorLevel.value, factorLevel.factorId, context.userId, factorLevel.id],
-      ),
-    ),
-  ),
+  batchUpdate: (factorLevels, context, tx = rep) => {
+    const columnSet = new pgp.helpers.ColumnSet(
+      [
+        '?id',
+        'value:raw',
+        'factor_id',
+        'modified_user_id',
+        'modified_date:raw'
+      ],
+      {table: 'factor_level_new'}
+    )
+    const data = factorLevels.map(factorLevel => ({
+      id: factorLevel.id,
+      value: `CAST(${pgp.as.json(factorLevel.value)} AS jsonb)`,
+      factor_id: factorLevel.factorId,
+      modified_user_id: context.userId,
+      modified_date: 'CURRENT_TIMESTAMP'
+    }))
+    const query = `${pgp.helpers.update(data, columnSet)} WHERE v.id = t.id RETURNING *`
+    return tx.any(query)
+  },
 
   remove: id => rep.oneOrNone('DELETE FROM factor_level_new WHERE id = $1 RETURNING id', id),
 

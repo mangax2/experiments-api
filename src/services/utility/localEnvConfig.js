d3ebdb72d4786886f7e445117a82777ac456aa3f
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2017-08-31 17:15:21 -0500

    Changed randomization to v2.

--- src/services/utility/localEnvConfig.js
+++ src/services/utility/localEnvConfig.js
@@ -23,7 +23,7 @@ const cfServices = {
   experimentsExternalAPIUrls: {
     value: {
       capacityRequestAPIUrl: 'https://api01-np.agro.services/capacity-request-api',
-      randomizationAPIUrl: 'https://api01-np.agro.services:443/randomizer/v1',
+      randomizationAPIUrl: 'https://api01-np.agro.services/randomizer/v2',
       profileAPIUrl: 'https://profile.velocity-np.ag/v2',
       experimentsTaggingAPIUrl: 'https://experiments-tagging-api-d.velocity-np.ag/experiments-tagging-api'
     },

39a1e8eb9befbcc035f5d6f383aa97114187ebf9
tlamb <tong.lambert@monsanto.com>
2017-11-02 11:07:20 -0500

    VE-2868: Update to add number of independent and dependent variable in the summary

--- db-scripts/ProdRelease_next.sql
+++ db-scripts/ProdRelease_next.sql
@@ -1,78 +1,7 @@
-ALTER TABLE public.group ADD set_id INTEGER;
+-- View: public.experiment_summary
 
-INSERT INTO ref_design_spec (name, created_user_id, created_date, modified_user_id, modified_date) VALUES ('Rep Dimension X', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
-INSERT INTO ref_design_spec (name, created_user_id, created_date, modified_user_id, modified_date) VALUES ('Rep Dimension Y', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
+-- DROP VIEW public.experiment_summary;
 
-create or replace function getNoDataSourceId() returns integer as
-$$
-declare x integer;
-begin
-  select id from ref_data_source_type where type = 'I do not have a data source' into x;
-  return x;
-end;
-$$ LANGUAGE plpgsql;
-
-insert into ref_data_source(name, ref_data_source_type_id, created_user_id, created_date, modified_user_id, modified_date)
-values('Custom', getNoDataSourceId(), 'PNWATT', CURRENT_TIMESTAMP, 'PNWATT', CURRENT_TIMESTAMP);
-
-insert into ref_data_source(name, ref_data_source_type_id, created_user_id, created_date, modified_user_id, modified_date)
-values('Composite', getNoDataSourceId(), 'PNWATT', CURRENT_TIMESTAMP, 'PNWATT', CURRENT_TIMESTAMP);
-
-drop function getNoDataSourceId();
-
-
-CREATE INDEX factor_level_new_factor_id
-  ON public.factor_level_new
-  USING btree
-  (factor_id);
-
-CREATE INDEX combination_element_new_factor_level_id
-  ON public.combination_element_new
-  USING btree
-  (factor_level_id);
-
-CREATE INDEX group_value_new_factor_level_id
-  ON public.group_value_new
-  USING btree
-  (factor_level_id);
-
-CREATE INDEX unit_treatment_id
-  ON public.unit
-  USING btree
-  (treatment_id);
-
-UPDATE ref_data_source SET name='None' WHERE name='Other';
-
-
-ALTER SEQUENCE factor_level_id_seq
-	OWNED BY public.factor_level_new.id;
-ALTER SEQUENCE factor_id_seq
-	OWNED BY public.factor_new.id;
-ALTER SEQUENCE combination_element_id_seq
-	OWNED BY public.combination_element_new.id;
-ALTER SEQUENCE group_value_id_seq
-	OWNED BY public.group_value_new.id;
-
-ALTER TABLE public.factor_level_new
-  ALTER COLUMN id SET DEFAULT nextval('factor_level_id_seq'::regclass);
-ALTER TABLE public.factor_new
-  ALTER COLUMN id SET DEFAULT nextval('factor_id_seq'::regclass);
-ALTER TABLE public.combination_element_new
-  ALTER COLUMN id SET DEFAULT nextval('combination_element_id_seq'::regclass);
-ALTER TABLE public.group_value_new
-  ALTER COLUMN id SET DEFAULT nextval('group_value_id_seq'::regclass);
-
-DROP TABLE public.group_value;
-DROP TABLE public.combination_element;
-DROP TABLE public.factor_level;
-DROP TABLE public.factor;
-
-ALTER TABLE public.factor_new RENAME TO factor;
-ALTER TABLE public.factor_level_new RENAME TO factor_level;
-ALTER TABLE public.combination_element_new RENAME TO combination_element;
-ALTER TABLE public.group_value_new RENAME TO group_value;
-
--- Alter view to use factor instead of factor_new
 CREATE OR REPLACE VIEW public.experiment_summary AS
  WITH treatment_numbers AS (
          SELECT e_1.id AS experiment_id,
@@ -119,7 +48,9 @@ CREATE OR REPLACE VIEW public.experiment_summary AS
     COALESCE(t.number_of_treatments, 0::bigint)::integer AS number_of_treatments,
     COALESCE(eu.number_of_experimental_units, 0::bigint)::integer AS number_of_experimental_units,
     COALESCE(us.number_of_unit_specs, 0::bigint)::integer AS number_of_unit_specs,
-    utn.name_of_unit_type
+    utn.name_of_unit_type,
+    COALESCE(dv.number_of_dependent_variables, 0::bigint)::integer AS number_of_dependent_variables,
+    COALESCE(f.number_of_factors, 0::bigint)::integer AS number_of_independent_variables
    FROM experiment e
      LEFT JOIN treatment_numbers t ON t.experiment_id = e.id
      LEFT JOIN dependent_variable_numbers dv ON dv.experiment_id = e.id
@@ -129,11 +60,8 @@ CREATE OR REPLACE VIEW public.experiment_summary AS
      LEFT JOIN unit_type_name utn ON utn.experiment_id = e.id;
 
 ALTER TABLE public.experiment_summary
-    OWNER TO experiments_user_s;
-
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.experiment_summary TO experiments_app_user;
-GRANT SELECT ON TABLE public.experiment_summary TO experiments_ro_user;
+  OWNER TO experiments_user_s;
 GRANT ALL ON TABLE public.experiment_summary TO experiments_user_s;
+GRANT SELECT ON TABLE public.experiment_summary TO experiments_ro_user;
 GRANT SELECT ON TABLE public.experiment_summary TO experiments_dev_app_user;
-
--- ******************************* The above statements have been run in NP **********************************
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE public.experiment_summary TO experiments_app_user;

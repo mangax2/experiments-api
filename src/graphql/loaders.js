8e6df7475c0b41179cbe2b70d79e4530e9ffc898
pnwatt <pnwatt@monsanto.com>
2018-01-31 15:37:49 -0600

    VE2932: Refactor

--- src/graphql/loaders.js
+++ src/graphql/loaders.js
@@ -1,164 +1,20 @@
 const DataLoader = require('dataloader')
 const db = require('../db/DbManager')
 
-function associatedFactorLevelBatchResolver(ids, tx) {
-  return db.factorLevelAssociation.batchFindAssociatedLevels(ids, tx)
-}
-
-function combinationElementBatchResolver(ids, tx) {
-  return db.combinationElement.batchFind(ids, tx)
-}
-
-function combinationElementByTreatmentIdsBatchResolver(ids, tx) {
-  return db.combinationElement.batchFindAllByTreatmentIds(ids, tx)
-}
-
-function dependentVariableBatchResolver(ids, tx) {
-  return db.dependentVariable.batchFind(ids, tx)
-}
-
-function dependentVariableByExperimentIdsBatchResolver(ids, tx) {
-  return db.dependentVariable.batchFindByExperimentId(ids, tx)
-}
-
-function designSpecDetailBatchResolver(ids, tx) {
-  return db.designSpecificationDetail.batchFind(ids, tx)
-}
-
-function designSpecDetailByExperimentIdsBatchResolver(ids, tx) {
-  return db.designSpecificationDetail.batchFindAllByExperimentId(ids, tx)
-}
-
-function experimentBatchResolver(ids, tx) {
+function experimentBatchLoaderCallback(ids, tx) {
   return db.experiments.batchFindExperimentOrTemplate(ids, false, tx)
 }
 
-function experimentsBatchResolver(tx) {
+function experimentsBatchLoaderCallback(tx) {
   return db.experiments.all(false, tx).then(data => [data])
 }
 
-function factorBatchResolver(ids, tx) {
-  return db.factor.batchFind(ids, tx)
-}
-
-function factorByExperimentIdsBatchResolver(ids, tx) {
-  return db.factor.batchFindByExperimentId(ids, tx)
-}
-
-function factorLevelBatchResolver(ids, tx) {
-  return db.factorLevel.batchFind(ids, tx)
-}
-
-function factorLevelByFactorIdsBatchResolver(ids, tx) {
-  return db.factorLevel.batchFindByFactorId(ids, tx)
-}
-
-function factorLevelAssociationBatchResolver(ids, tx) {
-  return db.factorLevelAssociation.batchFind(ids, tx)
-}
-
-function groupBatchResolver(ids, tx) {
-  return db.group.batchFind(ids, tx)
-}
-
-function groupByExperimentIdsBatchResolver(ids, tx) {
-  return db.group.batchFindAllByExperimentId(ids, tx)
-}
-
-function groupByParentIdsBatchResolver(ids, tx) {
-  return db.group.batchFindAllByParentId(ids, tx)
-}
-
-function groupValueBatchResolver(ids, tx) {
-  return db.groupValue.batchFindAllByGroupId(ids, tx)
-}
-
-function nestedFactorLevelBatchResolver(ids, tx) {
-  return db.factorLevelAssociation.batchFindNestedLevels(ids, tx)
-}
-
-function ownerBatchResolver(ids, tx) {
-  return db.owner.batchFind(ids, tx)
-}
-
-function ownersByExperimentIdsBatchResolver(ids, tx) {
-  return db.owner.batchFindByExperimentIds(ids, tx)
-}
-
-function refDataSourceBatchResolver(ids, tx) {
-  return db.refDataSource.batchFind(ids, tx)
-}
-
-function refDataSourceTypeBatchResolver(ids, tx) {
-  return db.refDataSourceType.batchFind(ids, tx)
-}
-
-function refDesignSpecBatchResolver(ids, tx) {
-  return db.refDesignSpecification.batchFind(ids, tx)
-}
-
-function refExperimentDesignBatchResolver(ids, tx) {
-  return db.experimentDesign.batchFind(ids, tx)
-}
-
-function refFactorTypeBatchResolver(ids, tx) {
-  return db.factorType.batchFind(ids, tx)
-}
-
-function refGroupTypeBatchResolver(ids, tx) {
-  return db.groupType.batchFind(ids, tx)
-}
-
-function refUnitSpecBatchResolver(ids, tx) {
-  return db.unitSpecification.batchFind(ids, tx)
-}
-
-function refUnitTypeBatchResolver(ids, tx) {
-  return db.unitType.batchFind(ids, tx)
-}
-
-function setBySetIdsBatchResolver(ids, tx) {
-  return db.group.batchFindAllBySetIds(ids, tx)
-}
-
-function templateBatchResolver(ids, tx) {
+function templateBatchLoaderCallback(ids, tx) {
   return db.experiments.batchFindExperimentOrTemplate(ids, true, tx)
 }
 
-function templatesBatchResolver(tx) {
+function templatesBatchLoaderCallback(tx) {
   return db.experiments.all(true, tx).then(data => [data])
-}
-
-// function setEntriesBySetIdsBatchResolver(ids, tx) {
-//   return db.unit.batchFindAllBySetIdsGQL(ids, tx)
-// }
-
-function treatmentBatchResolver(ids, tx) {
-  return db.treatment.batchFind(ids, tx)
-}
-
-function treatmentByExperimentIdsBatchResolver(ids, tx) {
-  return db.treatment.batchFindAllByExperimentId(ids, tx)
-}
-
-function unitBatchResolver(ids, tx) {
-  return db.unit.batchFindAllByGroupIdsAndGroupByGroupId(ids, tx)
-}
-
-function unitByExperimentIdBatchResolver(ids, tx) {
-  return db.unit.batchfindAllByExperimentIds(ids, tx)
-}
-
-function unitBySetIdBatchResolver(ids, tx) {
-  return db.unit.batchFindAllBySetIds(ids, tx)
-}
-
-function unitSpecDetailBatchResolver(ids, tx) {
-  return db.unitSpecificationDetail.batchFind(ids, tx)
-}
-
-function unitSpecDetailByExperimentIdsBatchResolver(ids, tx) {
-  return db.unitSpecificationDetail.batchFindAllByExperimentId(ids, tx)
 }
 
 const transactionalBatchResolverWrapper =
@@ -168,48 +24,50 @@ function createLoaders(tx) {
   const transactionalWrapper = transactionalBatchResolverWrapper(tx)
   const createDataLoader = batchResolver => new DataLoader(transactionalWrapper(batchResolver))
   return {
-    associatedFactorLevel: createDataLoader(associatedFactorLevelBatchResolver),
-    combinationElement: createDataLoader(combinationElementBatchResolver),
+    associatedFactorLevel:
+      createDataLoader(db.factorLevelAssociation.batchFindAssociatedLevels),
+    combinationElement: createDataLoader(db.combinationElement.batchFind),
     combinationElementByTreatmentIds:
-      createDataLoader(combinationElementByTreatmentIdsBatchResolver),
-    dependentVariable: createDataLoader(dependentVariableBatchResolver),
+      createDataLoader(db.combinationElement.batchFindAllByTreatmentIds),
+    dependentVariable: createDataLoader(db.dependentVariable.batchFind),
     dependentVariableByExperimentIds: createDataLoader(
-      dependentVariableByExperimentIdsBatchResolver),
-    designSpecDetail: createDataLoader(designSpecDetailBatchResolver),
-    designSpecDetailByExperimentIds: createDataLoader(designSpecDetailByExperimentIdsBatchResolver),
-    experiment: createDataLoader(experimentBatchResolver),
-    experiments: createDataLoader(experimentsBatchResolver),
-    factor: createDataLoader(factorBatchResolver),
-    factorByExperimentIds: createDataLoader(factorByExperimentIdsBatchResolver),
-    factorLevel: createDataLoader(factorLevelBatchResolver),
-    factorLevelByFactorIds: createDataLoader(factorLevelByFactorIdsBatchResolver),
-    factorLevelAssociation: createDataLoader(factorLevelAssociationBatchResolver),
-    group: createDataLoader(groupBatchResolver),
-    groupsByExperimentIds: createDataLoader(groupByExperimentIdsBatchResolver),
-    groupByParentIds: createDataLoader(groupByParentIdsBatchResolver),
-    groupValue: createDataLoader(groupValueBatchResolver),
-    nestedFactorLevel: createDataLoader(nestedFactorLevelBatchResolver),
-    owner: createDataLoader(ownerBatchResolver),
-    ownersByExperimentIds: createDataLoader(ownersByExperimentIdsBatchResolver),
-    refDataSource: createDataLoader(refDataSourceBatchResolver),
-    refDataSourceType: createDataLoader(refDataSourceTypeBatchResolver),
-    refDesignSpec: createDataLoader(refDesignSpecBatchResolver),
-    refExperimentDesign: createDataLoader(refExperimentDesignBatchResolver),
-    refFactorType: createDataLoader(refFactorTypeBatchResolver),
-    refGroupType: createDataLoader(refGroupTypeBatchResolver),
-    refUnitSpec: createDataLoader(refUnitSpecBatchResolver),
-    refUnitType: createDataLoader(refUnitTypeBatchResolver),
-    setBySetIds: createDataLoader(setBySetIdsBatchResolver),
-    template: createDataLoader(templateBatchResolver),
-    templates: createDataLoader(templatesBatchResolver),
-    // setEntriesBySetIds: createDataLoader(setEntriesBySetIdsBatchResolver),
-    treatment: createDataLoader(treatmentBatchResolver),
-    treatmentByExperimentIds: createDataLoader(treatmentByExperimentIdsBatchResolver),
-    unit: createDataLoader(unitBatchResolver),
-    unitByExperimentIds: createDataLoader(unitByExperimentIdBatchResolver),
-    unitsBySetId: createDataLoader(unitBySetIdBatchResolver),
-    unitSpecDetail: createDataLoader(unitSpecDetailBatchResolver),
-    unitSpecDetailByExperimentIds: createDataLoader(unitSpecDetailByExperimentIdsBatchResolver),
+      db.dependentVariable.batchFindByExperimentId),
+    designSpecDetail: createDataLoader(db.designSpecificationDetail.batchFind),
+    designSpecDetailByExperimentIds:
+      createDataLoader(db.designSpecificationDetail.batchFindAllByExperimentId),
+    experiment: createDataLoader(experimentBatchLoaderCallback),
+    experiments: createDataLoader(experimentsBatchLoaderCallback),
+    factor: createDataLoader(db.factor.batchFind),
+    factorByExperimentIds: createDataLoader(db.factor.batchFindByExperimentId),
+    factorLevel: createDataLoader(db.factorLevel.batchFind),
+    factorLevelByFactorIds: createDataLoader(db.factorLevel.batchFindByFactorId),
+    factorLevelAssociation: createDataLoader(db.factorLevelAssociation.batchFind),
+    group: createDataLoader(db.group.batchFind),
+    groupsByExperimentIds: createDataLoader(db.group.batchFindAllByExperimentId),
+    groupByParentIds: createDataLoader(db.group.batchFindAllByParentId),
+    groupValue: createDataLoader(db.groupValue.batchFindAllByGroupId),
+    nestedFactorLevel: createDataLoader(db.factorLevelAssociation.batchFindNestedLevels),
+    owner: createDataLoader(db.owner.batchFind),
+    ownersByExperimentIds: createDataLoader(db.owner.batchFindByExperimentIds),
+    refDataSource: createDataLoader(db.refDataSource.batchFind),
+    refDataSourceType: createDataLoader(db.refDataSourceType.batchFind),
+    refDesignSpec: createDataLoader(db.refDesignSpecification.batchFind),
+    refExperimentDesign: createDataLoader(db.experimentDesign.batchFind),
+    refFactorType: createDataLoader(db.factorType.batchFind),
+    refGroupType: createDataLoader(db.groupType.batchFind),
+    refUnitSpec: createDataLoader(db.unitSpecification.batchFind),
+    refUnitType: createDataLoader(db.unitType.batchFind),
+    setBySetIds: createDataLoader(db.group.batchFindAllBySetIds),
+    template: createDataLoader(templateBatchLoaderCallback),
+    templates: createDataLoader(templatesBatchLoaderCallback),
+    treatment: createDataLoader(db.treatment.batchFind),
+    treatmentByExperimentIds: createDataLoader(db.treatment.batchFindAllByExperimentId),
+    unit: createDataLoader(db.unit.batchFindAllByGroupIdsAndGroupByGroupId),
+    unitByExperimentIds: createDataLoader(db.unit.batchfindAllByExperimentIds),
+    unitsBySetId: createDataLoader(db.unit.batchFindAllBySetIds),
+    unitSpecDetail: createDataLoader(db.unitSpecificationDetail.batchFind),
+    unitSpecDetailByExperimentIds:
+      createDataLoader(db.unitSpecificationDetail.batchFindAllByExperimentId),
   }
 }
 

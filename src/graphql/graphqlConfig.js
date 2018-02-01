59a352b32c38165894ae420e5b51a1c1d36c696c
pnwatt <pnwatt@monsanto.com>
2018-01-31 22:36:40 -0600

    VE2932: Added validation rule to limit query depth.

--- src/graphql/graphqlConfig.js
+++ src/graphql/graphqlConfig.js
@@ -1,13 +1,37 @@
 import graphqlHTTP from 'express-graphql'
+import { GraphQLError } from 'graphql'
 import db from '../db/DbManager'
 import loaders from '../graphql/loaders'
 import schema from '../graphql/schema'
+
+function LimitQueryDepth(maxDepth) {
+  return (context) => {
+    let currentDepth = 0
+    return {
+      Field: {
+        enter: () => {
+          currentDepth += 1
+          if (currentDepth > maxDepth) {
+            context.reportError(new GraphQLError(
+              `Validation: Query has exceeded maximum allowed depth of ${maxDepth}`))
+          }
+        },
+        leave: () => {
+          currentDepth -= 1
+        },
+      },
+    }
+  }
+}
 
 function x(request, response) {
   return db.tx('GraphQLTransaction', (tx) => {
     const handler = graphqlHTTP({
       schema,
       context: { loaders: loaders.createLoaders(tx) },
+      // NOTE: Depth must be greater than schema depth or
+      // GraphiQL will fail to retrieve documentation.
+      validationRules: [LimitQueryDepth(10)],
       graphiql: true,
     })
     return handler(request, response)

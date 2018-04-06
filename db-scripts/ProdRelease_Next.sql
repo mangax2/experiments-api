3bc080bebd89c5fecf0dfa586ab8af9abc879ea8
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2018-04-06 11:26:39 -0500

    VE-3220: Fixed issues with excessive duplicate factor level checking.

--- db-scripts/ProdRelease_Next.sql
+++ db-scripts/ProdRelease_Next.sql
@@ -7,3 +7,9 @@ WITH question_type AS (
 INSERT INTO ref_data_source (name, ref_data_source_type_id, created_user_id, created_date, modified_user_id, modified_date)
 SELECT 'Question', qt.id, 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP
 FROM question_type qt;
+
+ALTER TABLE public.factor_level DROP CONSTRAINT factor_level_new_ak_1;
+
+ALTER TABLE public.factor_level
+    ADD CONSTRAINT factor_level_new_ak_1 UNIQUE (factor_id, value)
+    DEFERRABLE INITIALLY DEFERRED;

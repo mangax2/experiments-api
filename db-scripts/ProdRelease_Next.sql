7cce03d6974e88668d18cbfdf9316fd39e57c6ed
tlamb <tong.lambert@monsanto.com>
2018-04-10 11:59:43 -0500

    VE-3188: update sql query to update all placeholder to true

--- db-scripts/ProdRelease_Next.sql
+++ db-scripts/ProdRelease_Next.sql
@@ -9,40 +9,49 @@ SELECT 'Question', qt.id, 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP
 FROM question_type qt;
 
 
---update factor level value column for multiline items
-with itemsArray as (
-select fl.id as fl_id, json_array_elements(to_json(fl.value #>'{items}')) as value from factor_level fl
-where json_typeof(to_json(fl.value #>'{items}')) = 'array'
-),
-multilineitemsArrayWithPHAdded as (
-select ia.fl_id,
-jsonb_set(ia.value::jsonb, '{items, 0}', (ia.value #>'{items, 0}')::jsonb || '{"isPlaceholder": true}'::jsonb, true)  as value
-from itemsArray ia
-where (ia.value #>'{items, 0}')::jsonb ? 'isPlaceholder' = false
-and (ia.value)::jsonb ? 'items'
-),
-uniqueFLitemsWithPHArray as (
-select fl_id, jsonb_agg(to_json(value)) as value
-from multilineitemsArrayWithPHAdded as tab
-group by fl_id
-)
+--update factor level items with 'isPlaceholder = true' where isPlaceholder is not set
+with recursive
+    itemsArray as (
+      select id as fl_id,
+             case
+             when json_typeof(to_json(value #>'{items}')) = 'array' and (value #>'{items, 0}')::jsonb ? 'items'
+               then jsonb_array_elements(to_jsonb(value #>'{items}'))
+
+             when json_typeof(to_json(value #>'{items}')) = 'array'
+               then value
+             end as value
+      from factor_level
+  ),
+    itemsArrayWithPHAdded AS (
+    select fl_id, 0 as nr,
+                  case
+                  when (ia.value #>'{items, 0}')::jsonb ? 'isPlaceholder' = false
+                    then jsonb_set(ia.value::jsonb, '{items, 0}', (ia.value #>'{items, 0}')::jsonb || '{"isPlaceholder": true}'::jsonb, true)
+                  else
+                    ia.value
+                  end as value
+    from itemsArray ia
+    where (ia.value)::jsonb ? 'items'
+
+    UNION ALL
+
+    select fl_id, nr + 1,
+      case
+      when (jsonb_extract_path(value, 'items')->>(nr+1))::jsonb ? 'isPlaceholder' = false
+        then jsonb_set(value::jsonb, ('{items,' || nr+1 || '}')::text[], (jsonb_extract_path(value, 'items')->>(nr +1))::jsonb || '{"isPlaceholder": true}'::jsonb, true)
+      else
+        value
+      end as value
+    from itemsArrayWithPHAdded
+    where jsonb_array_length((value #>'{items}')::jsonb) > (nr+1)
+  ),
+    uniqueFLitemsWithPHArray as (
+      select fl_id, nr, jsonb_agg(to_json(value)) as value
+      from itemsArrayWithPHAdded as tab
+      where jsonb_array_length((value #>'{items}')::jsonb) = (nr+1)
+      group by fl_id, nr
+  )
 update factor_level as fl
 set value = jsonb_set(fl.value, '{items}', ua.value)
 from uniqueFLitemsWithPHArray ua
-where ua.fl_id = fl.id;
-
---update factor level value column for single line items
-with itemsArray as (
-select fl.id as fl_id, fl.value from factor_level fl
-where (fl.value #>'{items, 0}')::jsonb ? 'items' = false
-),
-itemsArrayWithPHAdded as (
-select ia.fl_id,
-jsonb_set(ia.value::jsonb, '{items, 0}', (ia.value #>'{items, 0}')::jsonb || '{"isPlaceholder": true}'::jsonb, true)  as value
-from itemsArray ia
-where (ia.value #>'{items, 0}')::jsonb ? 'isPlaceholder' = false
-)
-update factor_level as fl
-set value = ua.value
-from itemsArrayWithPHAdded ua
 where ua.fl_id = fl.id;

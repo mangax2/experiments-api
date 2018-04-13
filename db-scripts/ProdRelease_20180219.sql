9af97d1f8d25fd23f54cacf635ef0243e61cfa85
pnwatt <pnwatt@monsanto.com>
2018-04-13 14:16:33 -0500

    VE3188: Updated prod release DB script to remove statements already executed in prod.

--- db-scripts/ProdRelease_20180219.sql
+++ db-scripts/ProdRelease_20180219.sql
@@ -1,1 +1,11 @@
 ALTER TABLE experiment ADD COLUMN capacity_request_sync_date timestamp with time zone
+
+INSERT INTO ref_data_source_type (type, created_user_id, created_date, modified_user_id, modified_date) VALUES ('QandA', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
+
+WITH question_type AS (
+  SELECT id FROM ref_data_source_type
+  WHERE type='QandA'
+)
+INSERT INTO ref_data_source (name, ref_data_source_type_id, created_user_id, created_date, modified_user_id, modified_date)
+SELECT 'Question', qt.id, 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP
+FROM question_type qt;
--- db-scripts/ProdRelease_Next.sql
+++ db-scripts/ProdRelease_Next.sql
@@ -1,13 +1,3 @@
-INSERT INTO ref_data_source_type (type, created_user_id, created_date, modified_user_id, modified_date) VALUES ('QandA', 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP);
-
-WITH question_type AS (
-  SELECT id FROM ref_data_source_type
-  WHERE type='QandA'
-)
-INSERT INTO ref_data_source (name, ref_data_source_type_id, created_user_id, created_date, modified_user_id, modified_date)
-SELECT 'Question', qt.id, 'KMCCL', CURRENT_TIMESTAMP, 'KMCCL', CURRENT_TIMESTAMP
-FROM question_type qt;
-
 ALTER TABLE public.factor_level DROP CONSTRAINT factor_level_new_ak_1;
 
 ALTER TABLE public.factor_level

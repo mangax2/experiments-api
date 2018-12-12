6dd50e22d0eb59aae5160ef8cde721cecbb96c89
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2018-12-12 14:20:23 -0600

    Fixed a migration script.

--- migration-scripts/20181203_Exp61.sql
+++ migration-scripts/20181203_Exp61.sql
@@ -1,5 +1,5 @@
 ALTER TABLE location_association ADD COLUMN block INTEGER;
 
-ALTER TABLE location_association DROP CONSTRAINT location_association_experiment_id_location_number_key;
+ALTER TABLE location_association DROP CONSTRAINT location_association_experiment_id_location_key;
 
 ALTER TABLE location_association ADD CONSTRAINT location_association_experiment_id_location_number_block_key UNIQUE (experiment_id, location, block);

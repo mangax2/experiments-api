b65806f70c8db16112c9dd569326181bfbd718fe
kchit <kishore.chittamuru@monsanto.com>
2019-01-04 13:07:19 -0600

    EXP:75 :Changed from Dependent to Response and Factor to Treatmentvaraible in GraphQl/Swagger and routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -76,7 +76,7 @@ router.get('/experiments/:id/permissions', (req, res, next) => {
     .catch(err => next(err))
 })
 
-router.get('/experiments/:id/dependent-variables', (req, res, next) => {
+router.get('/experiments/:id/response-variables', (req, res, next) => {
   const { id } = req.params
   return new DependentVariableService().getDependentVariablesByExperimentId(id, false, req.context)
     .then(dependentVariables => res.json(dependentVariables))
@@ -92,17 +92,13 @@ router.get('/experiments/:id/variables', (req, res, next) => new FactorDependent
   .then(success => res.json(success))
   .catch(err => next(err)))
 
-router.get('/factors', (req, res, next) => new FactorService().getAllFactors()
+router.get('/treatment-variables', (req, res, next) => new FactorService().getAllFactors()
   .then(factors => res.json(factors))
   .catch(err => next(err)))
-router.get('/experiments/:id/factors', (req, res, next) => new FactorService().getFactorsByExperimentId(req.params.id, false, req.context)
+router.get('/experiments/:id/treatment-variables', (req, res, next) => new FactorService().getFactorsByExperimentId(req.params.id, false, req.context)
   .then(factors => res.json(factors))
   .catch(err => next(err)))
-router.get('/factors', (req, res, next) => new FactorService().getAllFactors()
-  .then(factors => res.json(factors))
-  .catch(err => next(err)))
-
-router.get('/factor-levels', (req, res, next) => new FactorLevelService().getAllFactorLevels()
+router.get('/treatment-variable-levels', (req, res, next) => new FactorLevelService().getAllFactorLevels()
   .then(factorLevels => res.json(factorLevels))
   .catch(err => next(err)))
 router.get('/experiments/:id/treatments', (req, res, next) => new TreatmentDetailsService().getAllTreatmentDetails(req.params.id, false, req.context)
@@ -176,7 +172,7 @@ router.post('/experiments/:id/unit-specification-details', (req, res, next) => n
   .then(value => res.json(value))
   .catch(err => next(err)))
 
-router.post('/preferences/factors/lists', (req, res, next) => new ListsService(new PreferencesService()).setUserLists(req.body.userId, req.body.listIds, req.headers, req.context)
+router.post('/preferences/treatment-variables/lists', (req, res, next) => new ListsService(new PreferencesService()).setUserLists(req.body.userId, req.body.listIds, req.headers, req.context)
   .then(data => res.status(200).json(data))
   .catch(err => next(err)))
 
@@ -239,7 +235,7 @@ router.get('/templates/:id/permissions', (req, res, next) => {
     .catch(err => next(err))
 })
 
-router.get('/templates/:id/dependent-variables', (req, res, next) => {
+router.get('/templates/:id/response-variables', (req, res, next) => {
   const { id } = req.params
   return new DependentVariableService().getDependentVariablesByExperimentId(id, true, req.context)
     .then(dependentVariables => res.json(dependentVariables))
@@ -250,7 +246,7 @@ router.get('/templates/:id/variables', (req, res, next) => new FactorDependentCo
   .then(success => res.json(success))
   .catch(err => next(err)))
 
-router.get('/templates/:id/factors', (req, res, next) => new FactorService().getFactorsByExperimentId(req.params.id, true, req.context)
+router.get('/templates/:id/treatment-variables', (req, res, next) => new FactorService().getFactorsByExperimentId(req.params.id, true, req.context)
   .then(factors => res.json(factors))
   .catch(err => next(err)))
 

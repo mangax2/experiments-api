d8deb0c4211988072fa1898f8ba82103853a03a6
tlamb <tong.lambert@monsanto.com>
2019-01-15 13:51:10 -0600

    Exp-107: fix the postgres query causes deadlock on clearing sets

--- src/repos/unit.js
+++ src/repos/unit.js
@@ -130,7 +130,7 @@ class unitRepo {
 
     return tx.none('UPDATE unit u SET set_entry_id = NULL\n' +
       'FROM treatment t, location_association la\n' +
-      'WHERE u.treatment_id = t.id AND u.location = la.location AND u.block IS NOT DISTINCT FROM la.block AND la.set_id = $1', setId)
+      'WHERE u.treatment_id = t.id AND t.experiment_id = la.experiment_id AND u.location = la.location AND u.block IS NOT DISTINCT FROM la.block AND la.set_id = $1', setId)
   }
 
   @setErrorCode('5JI000')

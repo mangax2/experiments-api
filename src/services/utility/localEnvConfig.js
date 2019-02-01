b0a2c625ac54ffe9120ee4a098e76ef2294a3173
kchit <kishore.chittamuru@monsanto.com>
2019-02-01 13:20:45 -0600

    EXP:68 : API chnages for cut over to new API

--- src/services/utility/localEnvConfig.js
+++ src/services/utility/localEnvConfig.js
@@ -23,7 +23,7 @@ const cfServices = {
       setsAPIUrl: 'https://api01-np.agro.services/sets-api/v2',
       materialListsAPIUrl: 'https://api01-np.agro.services/material-lists-api/v1',
       velocityMessagingAPIUrl: 'https://messaging.velocity-np.ag/v5',
-      randomizationAPIUrl: 'https://api01-np.agro.services/randomizer/v3'
+      randomizationAPIUrl: 'https://velocity-np.ag/randomize-treatments-api'
     },
   },
   experimentsKafka:{

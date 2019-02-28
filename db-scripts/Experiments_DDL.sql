b88866da6886ce39bd5f1fd9aaaa7497feae4fef
kchit <kishore.chittamuru@monsanto.com>
2019-02-28 13:06:26 -0600

    EXP:65 : Modified the database script

--- db-scripts/Experiments_DDL.sql
+++ db-scripts/Experiments_DDL.sql
@@ -408,31 +408,29 @@ CREATE TABLE group_value
 );
 
 --analysis_model table
+
 CREATE TABLE public.analysis_model
 (
     id integer NOT NULL DEFAULT nextval('analysis_model_id_seq'::regclass),
     experiment_id integer NOT NULL,
     analysis_model_code character varying COLLATE pg_catalog."default" NOT NULL,
     analysis_model_sub_type character varying COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT analysis_model_pkey PRIMARY KEY (id)
+    CONSTRAINT analysis_model_pkey PRIMARY KEY (id),
+    CONSTRAINT experiment_uniq_key UNIQUE (experiment_id),
+    CONSTRAINT experiment_id FOREIGN KEY (experiment_id)
+        REFERENCES public.experiment (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public.analysis_model
-    OWNER to experiments_user_s;
-
-GRANT DELETE, INSERT, UPDATE ON TABLE public.analysis_model TO auto_sql_user;
-
-GRANT INSERT, UPDATE, DELETE ON TABLE public.analysis_model TO experiments_dev_app_user;
-
-GRANT ALL ON TABLE public.analysis_model TO experiments_user_s;
-
-GRANT SELECT ON TABLE public.analysis_model TO PUBLIC;
-
-
+CREATE INDEX analysis_model_experiment_id
+ ON public.analysis_model USING btree
+  (experiment_id)
+  TABLESPACE pg_default;
 
 -- Drop Hypothesis Table
 --Alter Subject Type Column to Descriptiuon in Experiment Table

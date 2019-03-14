0a63d749df99dc6dab7acc0ccf389e916220cdaa
KMCCL <kyle.mcclendon@monsanto.com>
2019-03-14 10:26:33 -0500

    EXP-115: Fixing migration script, again

--- migration-scripts/20190306_EXP-115_control-types.sql
+++ migration-scripts/20190306_EXP-115_control-types.sql
@@ -1,7 +1,7 @@
 ALTER TABLE treatment ADD COLUMN control_types text[];
 
 UPDATE treatment
-SET control_types = CASE WHEN current_database() = 'experiments_prod' THEN '{"af6d651d-01c0-4083-8dca-ffed57fb0bcc"}' ELSE '{"340ec050-9827-48f2-8c43-8e73913cb286"}' END
+SET control_types = CASE WHEN current_database() = 'experiments_prod' THEN '{"af6d651d-01c0-4083-8dca-ffed57fb0bcc"}'::text[] ELSE '{"340ec050-9827-48f2-8c43-8e73913cb286"}'::text[] END
 WHERE is_control = TRUE;
 
 ALTER TABLE treatment DROP COLUMN is_control;

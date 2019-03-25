26c909d34cac19c3511148bb2188fe0375ae273a
tlamb <tong.lambert@monsanto.com>
2019-03-25 11:45:27 -0500

    Exp-166: treatment validation failed due to besides all treatmetns are validated, treatments for updates and add are validated as well.

--- src/validations/TreatmentValidator.js
+++ src/validations/TreatmentValidator.js
@@ -214,7 +214,7 @@ class TreatmentValidator extends SchemaValidator {
       return Promise.reject(
         AppError.badRequest('Treatment request object needs to be an array', undefined, getFullErrorCode('3F2001')))
     }
-    return this.validateBlockValue(treatmentDTOs)
+    return Promise.resolve()
   }
 
   @setErrorCode('3F9000')

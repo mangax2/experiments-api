c194fe835b062d42d491a848eebdf4384c0255af
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2019-10-23 16:04:34 -0500

    EXP-354: Fixed improper spaces.

--- migration-scripts/20191016_EXP-354_add-auditing.sql
+++ migration-scripts/20191016_EXP-354_add-auditing.sql
@@ -1,5 +1,5 @@
-SELECT audit.audit_table(tables.table_name)
-FROM (
-       SELECT table_catalog, table_schema, table_name FROM information_schema.tables where table_schema = 'public'
-       and table_name not in ('unit')
-     )  as tables ;
+SELECT audit.audit_table(tables.table_name)
+FROM (
+       SELECT table_catalog, table_schema, table_name FROM information_schema.tables where table_schema = 'public'
+       and table_name not in ('unit')
+     )  as tables ;

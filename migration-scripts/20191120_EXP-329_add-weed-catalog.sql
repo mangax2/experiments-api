6b2134d4f2d39001cc413dc02f6801c70d8f8d92
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2019-11-21 10:28:40 -0600

    EXP-329: Corrected the Weeds Catalog name/id.

--- migration-scripts/20191120_EXP-329_add-weed-catalog.sql
+++ migration-scripts/20191120_EXP-329_add-weed-catalog.sql
@@ -1,2 +1,2 @@
 insert into ref_data_source(name, ref_id, ref_data_source_type_id, created_user_id, created_date, modified_user_id, modified_date)
-values('Weed Catalog', 'WEED', (select id from ref_data_source_type where type = 'Catalog'), 'migration', current_timestamp, 'migration', current_timestamp);
+values('Weeds Catalog', 'WEEDS', (select id from ref_data_source_type where type = 'Catalog'), 'migration', current_timestamp, 'migration', current_timestamp);

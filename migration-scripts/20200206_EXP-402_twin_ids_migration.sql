381e9b735cb6d7d72b4749d9b61a8e7e2cd2817b
Ryan Gamel <gcsxl@monsanto.com>
2020-02-10 15:30:02 -0600

    EXP-402: twin ids migration

---
+++ migration-scripts/20200206_EXP-402_twin_ids_migration.sql
@@ -0,0 +1,380 @@
+create temp table twins(original_id: text, new_id: text);
+-- weird off-batch from 12_18 slack post
+insert into twins(original_id, new_id) values('BBORD', 'BBORD9');
+insert into twins(original_id, new_id) values('BEORR', 'BEORR9');
+insert into twins(original_id, new_id) values('BJACO', 'BJACO9');
+-- 1 22 batch
+insert into twins(original_id, new_id) values('AANDERSON', 'AANDERSO');
+insert into twins(original_id, new_id) values('ASULLIVAN', 'ASULLIVA');
+insert into twins(original_id, new_id) values('ALANGEDIJK', 'ALANGEDI');
+insert into twins(original_id, new_id) values('ADUCHNOWSKI', 'ADUCHNOW');
+insert into twins(original_id, new_id) values('AOPPELAAR', 'AOPPELAA');
+insert into twins(original_id, new_id) values('ABOEKHOUDT', 'ABOEKHOU');
+insert into twins(original_id, new_id) values('BLINGBEEK', 'BLINGBEE');
+insert into twins(original_id, new_id) values('BBLOEMENDAAL', 'BBLOEMEN');
+insert into twins(original_id, new_id) values('BAPHINAIPRAS', 'BAPHINAI');
+insert into twins(original_id, new_id) values('CFOXWORTHY', 'CFOXWORT');
+insert into twins(original_id, new_id) values('CBLOEMENDAAL', 'CBLOEMEN');
+insert into twins(original_id, new_id) values('DHALLBERG', 'DHALLBER');
+insert into twins(original_id, new_id) values('DSCACCHETTI', 'DSCACCHE');
+insert into twins(original_id, new_id) values('DPACQUING', 'DPACQUIN');
+insert into twins(original_id, new_id) values('DMALEWADIKAR', 'DMALEWAD');
+insert into twins(original_id, new_id) values('DSTARLING', 'DSTARLIN');
+insert into twins(original_id, new_id) values('DCHIARIOTTI', 'DCHIARIO');
+insert into twins(original_id, new_id) values('DRJOHNSON', 'DRJOHNSO');
+insert into twins(original_id, new_id) values('EWESTFALL', 'EWESTFAL');
+insert into twins(original_id, new_id) values('EFILIPCSEI', 'EFILIPCS');
+insert into twins(original_id, new_id) values('FALBITRES', 'FALBITRE');
+insert into twins(original_id, new_id) values('FCOSTANTI', 'FCOSTANT');
+insert into twins(original_id, new_id) values('FFRIGYESI', 'FFRIGYES');
+insert into twins(original_id, new_id) values('FDELLAROCCA', 'FDELLARO');
+insert into twins(original_id, new_id) values('FBERTRAND', 'FBERTRAN');
+insert into twins(original_id, new_id) values('FSHANEYFELT', 'FSHANEYF');
+insert into twins(original_id, new_id) values('GMUCSINYI', 'GMUCSINY');
+insert into twins(original_id, new_id) values('GSCHOLTEN', 'GSCHOLTE');
+insert into twins(original_id, new_id) values('GKARRUPPANNA', 'GKARRUPP');
+insert into twins(original_id, new_id) values('GGASPERIN', 'GGASPERI');
+insert into twins(original_id, new_id) values('KWAID', 'KWAID9');
+-- 1 29 batch
+insert into twins(original_id, new_id) values('ACOLECCHIA', 'ACOLECCH');
+insert into twins(original_id, new_id) values('ANRAMIREZ', 'ANRAMIRE');
+insert into twins(original_id, new_id) values('BMCCARTHY', 'BMCCARTH');
+insert into twins(original_id, new_id) values('BMCCARTHY', 'BMCCARTH');
+insert into twins(original_id, new_id) values('CGONZALEZ', 'CGONZALE');
+insert into twins(original_id, new_id) values('EESCLASANS', 'EESCLASA');
+insert into twins(original_id, new_id) values('GBERNARDEAU', 'GBERNARD');
+insert into twins(original_id, new_id) values('GBERNARDEAU', 'GBERNARD');
+insert into twins(original_id, new_id) values('IROMANINI', 'IROMANIN');
+insert into twins(original_id, new_id) values('IBROERSEN', 'IBROERSE');
+insert into twins(original_id, new_id) values('JHOOGSTRATEN', 'JHOOGSTR');
+insert into twins(original_id, new_id) values('JBEAIRSTO', 'JBEAIRST');
+insert into twins(original_id, new_id) values('JOCALDERON', 'JOCALDER');
+insert into twins(original_id, new_id) values('JCUSVELLER', 'JCUSVELL');
+insert into twins(original_id, new_id) values('JSCHOLTEN', 'JSCHOLTE');
+insert into twins(original_id, new_id) values('JMCINTIER', 'JMCINTIE');
+insert into twins(original_id, new_id) values('JGONZALEZ', 'JGONZALE');
+insert into twins(original_id, new_id) values('JMGONZALEZ', 'JMGONZAL');
+insert into twins(original_id, new_id) values('JUSANTIAGO', 'JUSANTIA');
+insert into twins(original_id, new_id) values('KCASTILLO', 'KCASTILL');
+insert into twins(original_id, new_id) values('KHOOGLAND', 'KHOOGLAN');
+insert into twins(original_id, new_id) values('KCRAWFORD', 'KCRAWFOR');
+insert into twins(original_id, new_id) values('KKAMPHORST', 'KKAMPHOR');
+insert into twins(original_id, new_id) values('LDOUILLARD', 'LDOUILLA');
+insert into twins(original_id, new_id) values('LSURRIBAS', 'LSURRIBA');
+insert into twins(original_id, new_id) values('LCARRANZA', 'LCARRANZ');
+insert into twins(original_id, new_id) values('LGEERAERTS', 'LGEERAER');
+insert into twins(original_id, new_id) values('LLOMBARDI', 'LLOMBARD');
+insert into twins(original_id, new_id) values('LCOPELAND', 'LCOPELAN');
+insert into twins(original_id, new_id) values('LPIMPAENG', 'LPIMPAEN');
+insert into twins(original_id, new_id) values('MWIJDENES', 'MWIJDENE');
+insert into twins(original_id, new_id) values('MSWINKELS', 'MSWINKEL');
+insert into twins(original_id, new_id) values('MHERNANDEZ', 'MHERNAND');
+insert into twins(original_id, new_id) values('MACARMONA', 'MACARMON');
+insert into twins(original_id, new_id) values('MANTONISE', 'MANTONIS');
+insert into twins(original_id, new_id) values('MRUEDISUELI', 'MRUEDISU');
+insert into twins(original_id, new_id) values('MCASTILLO', 'MCASTILL');
+insert into twins(original_id, new_id) values('MDUREPAIRE', 'MDUREPAI');
+insert into twins(original_id, new_id) values('MMAYBERRY', 'MMAYBERR');
+insert into twins(original_id, new_id) values('MPHILLIPP', 'MPHILLIP');
+insert into twins(original_id, new_id) values('MRONCAGLIA', 'MRONCAGL');
+insert into twins(original_id, new_id) values('MEDELENBOSCH', 'MEDELENB');
+insert into twins(original_id, new_id) values('MSANDOVAL', 'MSANDOVA');
+insert into twins(original_id, new_id) values('MCHONGPERMKL', 'MCHONGPE');
+insert into twins(original_id, new_id) values('MKUHEYLAN', 'MKUHEYLA');
+insert into twins(original_id, new_id) values('NBERGAMINI', 'NBERGAMI');
+insert into twins(original_id, new_id) values('NKOOMANKAS', 'NKOOMANK');
+insert into twins(original_id, new_id) values('DSRIN', 'DSRIN9');
+-- 2 5 batch
+insert into twins(original_id, new_id) values('ABALS', 'ABALS9');
+insert into twins(original_id, new_id) values('AMITCHELL', 'AMITCHEL');
+insert into twins(original_id, new_id) values('BFIEL', 'BFIEL9');
+insert into twins(original_id, new_id) values('CECASTILLO', 'CECASTIL');
+insert into twins(original_id, new_id) values('EMUSY', 'EMUSY9');
+insert into twins(original_id, new_id) values('JTHOMASSON', 'JTHOMASS');
+insert into twins(original_id, new_id) values('JVELTHOEN', 'JVELTHOE');
+insert into twins(original_id, new_id) values('JWETTLESON', 'JWETTLES');
+insert into twins(original_id, new_id) values('JAMONRABAL', 'JAMONRAB');
+insert into twins(original_id, new_id) values('KREYNOLDS', 'KREYNOLD');
+insert into twins(original_id, new_id) values('LPATRICIO', 'LPATRICI');
+insert into twins(original_id, new_id) values('LPALLOTTINI', 'LPALLOTT');
+insert into twins(original_id, new_id) values('MRODRIGUES', 'MRODRIGU');
+insert into twins(original_id, new_id) values('MHEIJNINGEN', 'MHEIJNIN');
+insert into twins(original_id, new_id) values('MDITHGASORN', 'MDITHGAS');
+insert into twins(original_id, new_id) values('RPOOSAENG', 'RPOOSAEN');
+insert into twins(original_id, new_id) values('NRONCORONI', 'NRONCORO');
+insert into twins(original_id, new_id) values('NVALLENTGOED', 'NVALLENT');
+insert into twins(original_id, new_id) values('SKOBERLEIN', 'SKOBERLE');
+insert into twins(original_id, new_id) values('PBERENGUEL', 'PBERENGU');
+insert into twins(original_id, new_id) values('PGERRITSEN', 'PGERRITS');
+insert into twins(original_id, new_id) values('PVERHAEGH', 'PVERHAEG');
+insert into twins(original_id, new_id) values('PPHURMSUP', 'PPHURMSU');
+insert into twins(original_id, new_id) values('PBOONSOPHIN', 'PBOONSOP');
+insert into twins(original_id, new_id) values('PLLONGARINI', 'PLLONGAR');
+insert into twins(original_id, new_id) values('RMALDONADO', 'RMALDONA');
+insert into twins(original_id, new_id) values('RDFRANCESCO', 'RDFRANCE');
+insert into twins(original_id, new_id) values('RCARRILLO', 'RCARRILL');
+insert into twins(original_id, new_id) values('RDIJKSTRA', 'RDIJKSTR');
+insert into twins(original_id, new_id) values('RBLACKLEY', 'RBLACKLE');
+insert into twins(original_id, new_id) values('RMARCATOMA', 'RMARCATO');
+insert into twins(original_id, new_id) values('SMARTINEZ', 'SMARTINE');
+insert into twins(original_id, new_id) values('SHERNANDEZ', 'SHERNAND');
+insert into twins(original_id, new_id) values('SHENDRICKS', 'SHENDRIC');
+insert into twins(original_id, new_id) values('SALTAMIRANO', 'SALTAMIR');
+insert into twins(original_id, new_id) values('SCALDWELL', 'SCALDWEL');
+insert into twins(original_id, new_id) values('SOMARTINEZ', 'SOMARTIN');
+insert into twins(original_id, new_id) values('SRACHSINGHO', 'SRACHSIN');
+insert into twins(original_id, new_id) values('SCHEEWAWIRIY', 'SCHEEWAW');
+insert into twins(original_id, new_id) values('WBOONCHUEN', 'WBOONCHU');
+insert into twins(original_id, new_id) values('TLOEFFLER', 'TLOEFFLE');
+insert into twins(original_id, new_id) values('TTHERIAULT', 'TTHERIAU');
+insert into twins(original_id, new_id) values('UKULKARNI', 'UKULKARN');
+insert into twins(original_id, new_id) values('VMONTANARI', 'VMONTANA');
+insert into twins(original_id, new_id) values('WMALDEGEM', 'WMALDEGE');
+insert into twins(original_id, new_id) values('WEDELENBOSCH', 'WEDELENB');
+insert into twins(original_id, new_id) values('ZBELTETON', 'ZBELTETO');
+insert into twins(original_id, new_id) values('ETROS', 'ETROS9');
+
+UPDATE public.owner
+SET user_ids = ARRAY(
+    SELECT COALESCE(t.new_id, old_name.name) FROM (
+        SELECT UNNEST(o2.user_ids) AS name FROM public.owner o2 WHERE o2.id = owner.id
+    ) AS old_name
+    LEFT OUTER JOIN twins t ON old_name.name = t.original_id
+);
+
+UPDATE analysis_model
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE analysis_model
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE public.block
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE public.block
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE combination_element
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE combination_element
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE comment
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE comment
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE dependent_variable
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE dependent_variable
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE design_spec_detail
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE design_spec_detail
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE experiment
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE experiment
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE factor
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE factor
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE factor_level
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE factor_level
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE factor_level_association
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE factor_level_association
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE public.owner
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE public.owner
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_data_source
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_data_source
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_data_source_type
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_data_source_type
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_design_spec
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_design_spec
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_experiment_design
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_experiment_design
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_factor_type
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_factor_type
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_level_object_type
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_level_object_type
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_unit_spec
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_unit_spec
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE ref_unit_type
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE ref_unit_type
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE treatment
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE treatment
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE treatment_block
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE treatment_block
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE unit
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE unit
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE unit_spec_detail
+SET created_user_id = new_id
+FROM twins
+WHERE created_user_id = original_id;
+
+UPDATE unit_spec_detail
+SET modified_user_id = new_id
+FROM twins
+WHERE modified_user_id = original_id;
+
+UPDATE audit.graphql_audit
+SET user_id = new_id
+FROM twins
+WHERE user_id = original_id;
+
+DROP TABLE twins;

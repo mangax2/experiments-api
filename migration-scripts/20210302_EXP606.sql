8109010420cab78661c2a8fe025c0d615c578e21
Ryan Gamel <gcsxl@monsanto.com>
2021-03-04 08:50:02 -0600

    feat(EXP-606): correct casing and punctuation in migration script

--- migration-scripts/20210302_EXP606.sql
+++ migration-scripts/20210302_EXP606.sql
@@ -1,5 +1,5 @@
 INSERT INTO ref_design_spec(name, created_user_id, created_date, modified_user_id, modified_date)
-VALUES('Are all blocks at all locations?', 'migration', current_timestamp, 'migration', current_timestamp);
+VALUES('Are All Blocks At All Locations', 'migration', current_timestamp, 'migration', current_timestamp);
 
 INSERT INTO design_spec_detail(experiment_id, value, ref_design_spec_id, created_user_id, created_date, modified_user_id, modified_date)
 SELECT experiment_id, true, (SELECT id FROM ref_design_spec WHERE name = 'Are All Blocks At All Locations'), 'migration', current_timestamp, 'migration', current_timestamp

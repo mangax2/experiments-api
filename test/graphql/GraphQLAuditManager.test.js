9ce062bf4a30c9f0c320cc24f18140ce05109f1b
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2021-04-12 12:54:08 -0500

    EXP-760: Moved graphQL logging into batches outside of request to increase performance.

---
+++ test/graphql/GraphQLAuditManager.test.js
@@ -0,0 +1,86 @@
+import GraphQLAuditManager from '../../src/graphql/GraphQLAuditManager'
+import db from '../../src/db/DbManager'
+import { mockReject, mockResolve } from '../jestUtil'
+
+describe('GraphQLAuditManager', () => {
+  describe('logRequest', () => {
+    test('adds the request to the queue', () => {
+      GraphQLAuditManager.queue = []
+
+      GraphQLAuditManager.logRequest('query', 'userId', 'client')
+
+      expect(GraphQLAuditManager.queue.length).toEqual(1)
+      expect(GraphQLAuditManager.queue[0].query).toEqual('query')
+      expect(GraphQLAuditManager.queue[0].userId).toEqual('userId')
+      expect(GraphQLAuditManager.queue[0].clientId).toEqual('client')
+    })
+  })
+
+  describe('saveLogs', () => {
+    test('calls batchCreate with the queries and clears the queue on success', async () => {
+      const queries = [{ query: 'query 1' }, { query: 'query 2' }]
+      db.graphqlAudit.batchCreate = mockResolve()
+      GraphQLAuditManager.queue = queries
+
+      await GraphQLAuditManager.saveLogs()
+
+      expect(db.graphqlAudit.batchCreate).toHaveBeenCalledWith(queries)
+      expect(GraphQLAuditManager.queue).toEqual([])
+    })
+
+    test('calls batchCreate with the queries and does not clear the queue on failure', async () => {
+      const queries = [{ query: 'query 1' }, { query: 'query 2' }]
+      db.graphqlAudit.batchCreate = mockReject()
+      GraphQLAuditManager.queue = queries
+
+      await GraphQLAuditManager.saveLogs()
+
+      expect(db.graphqlAudit.batchCreate).toHaveBeenCalledWith(queries)
+      expect(GraphQLAuditManager.queue).toEqual(queries)
+    })
+
+    test('does not call batchCreate when no queries to save', async () => {
+      db.graphqlAudit.batchCreate = mockResolve()
+      GraphQLAuditManager.queue = []
+
+      await GraphQLAuditManager.saveLogs()
+
+      expect(db.graphqlAudit.batchCreate).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('startInterval', () => {
+    afterEach(() => {
+      clearInterval(GraphQLAuditManager.intervalHandle)
+      GraphQLAuditManager.intervalHandle = null
+    })
+
+    test('sets the intervalHandle', () => {
+      GraphQLAuditManager.startInterval()
+
+      expect(GraphQLAuditManager.intervalHandle).not.toBe(null)
+    })
+  })
+
+  describe('endInterval', () => {
+    let originalSaveLogs
+
+    beforeEach(() => {
+      originalSaveLogs = GraphQLAuditManager.saveLogs
+    })
+
+    afterEach(() => {
+      GraphQLAuditManager.saveLogs = originalSaveLogs
+    })
+
+    test('clears the interval and calls saveLogs', async () => {
+      GraphQLAuditManager.saveLogs = mockResolve()
+      GraphQLAuditManager.intervalHandle = 12345
+
+      await GraphQLAuditManager.endInterval()
+
+      expect(GraphQLAuditManager.intervalHandle).toBe(null)
+      expect(GraphQLAuditManager.saveLogs).toHaveBeenCalled()
+    })
+  })
+})

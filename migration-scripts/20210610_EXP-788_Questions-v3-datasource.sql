17241eff0384209786f81f2e5037ee35b7db0714
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2021-06-10 13:04:59 -0500

    EXP-788: Script to add QandAv3 data source.

---
+++ migration-scripts/20210610_EXP-788_Questions-v3-datasource.sql
@@ -0,0 +1,4 @@
+INSERT INTO ref_data_source (name, ref_data_source_type_id, created_user_id, modified_user_id, created_date, modified_date)
+SELECT 'Questions (v3)', id, 'JGORD1', 'JGORD1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+FROM ref_data_source_type
+WHERE type = 'QandA';

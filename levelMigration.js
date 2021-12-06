60ab0e2ee8479d0f9437db54df3434f5c7106846
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2021-12-06 11:21:36 -0600

    EXP-909: Refactored to async/await and added instructions.

--- levelMigration.js
+++ levelMigration.js
@@ -10,7 +10,7 @@ const getFactorLevelLines = (factorLevelValue) => {
   return [factorLevelValue.items]
 }
 
-const createFactorProperties = (factorProperties, tx) => {
+const createFactorProperties = async (factorProperties, tx) => {
   const columnSet = new pgp.helpers.ColumnSet(
     [
       'id:raw',
@@ -43,11 +43,11 @@ const createFactorProperties = (factorProperties, tx) => {
 
   const query1 = `DROP TABLE IF EXISTS temp_insert_factor_properties_for_level; CREATE TEMP TABLE temp_insert_factor_properties_for_level AS TABLE factor_properties_for_level WITH NO DATA; ${pgp.helpers.insert(values, columnSet)};`
   const query2 = 'INSERT INTO factor_properties_for_level SELECT * FROM temp_insert_factor_properties_for_level RETURNING id'
-  return tx.query(query1)
-    .then(() => tx.any(query2))
+  await tx.query(query1)
+  return tx.any(query2)
 }
 
-const createFactorLevelDetails = (factorLevelDetails, tx) => {
+const createFactorLevelDetails = async (factorLevelDetails, tx) => {
   const columnSet = new pgp.helpers.ColumnSet(
     [
       'id:raw',
@@ -82,83 +82,65 @@ const createFactorLevelDetails = (factorLevelDetails, tx) => {
 
   const query1 = `DROP TABLE IF EXISTS temp_insert_factor_level_details; CREATE TEMP TABLE temp_insert_factor_level_details AS TABLE factor_level_details WITH NO DATA; ${pgp.helpers.insert(values, columnSet)};`
   const query2 = 'INSERT INTO factor_level_details SELECT * FROM temp_insert_factor_level_details RETURNING id'
-  return tx.query(query1)
-    .then(() => tx.any(query2))
+  await tx.query(query1)
+  return tx.any(query2)
 }
 
-const doPropertyMigration = db =>
-  db.tx('propertyMigration', tx =>
-    tx.any('SELECT DISTINCT e.id FROM experiment e INNER JOIN factor f ON e.id = f.experiment_id LEFT JOIN factor_properties_for_level fpfl ON f.id = fpfl.factor_id WHERE fpfl.id IS NULL LIMIT 10')
-      .then((experiments) => {
-        if (experiments && experiments.length) {
-          const experimentIds = experiments.map(e => e.id)
-          console.info('migrating experiment ids', experimentIds)
-          return tx.any('SELECT fl.factor_id, value FROM factor f INNER JOIN (SELECT factor_id, MIN(id) as factor_level_id FROM factor_level GROUP BY factor_id) AS joiner ON f.id = joiner.factor_id INNER JOIN factor_level fl ON joiner.factor_level_id = fl.id WHERE f.experiment_id in ($1:csv)', [experimentIds])
-            .then((factorLevels) => {
-              const factorProperties = factorLevels.flatMap((factorLevel) => {
-                const factorLevelLines = getFactorLevelLines(factorLevel.value)[0]
-                return factorLevelLines
-                  .map((detail, index) => ({
-                    ...detail,
-                    factorId: factorLevel.factor_id,
-                    questionCode: detail.multiQuestionTag ? undefined : detail.questionCode,
-                    columnNumber: index + 1,
-                  }))
-              })
-              return createFactorProperties(factorProperties, tx)
-            })
-            .then(() => experimentIds.length)
-        }
-        return 0
-      }))
-    .then((numberRowsMigrated) => {
-      if (numberRowsMigrated > 0) {
-        doPropertyMigration(db)
-      }
-    })
+const doPropertyMigration = async db =>
+  db.tx('propertyMigration', async (tx) => {
+    const experiments = await tx.any('SELECT DISTINCT e.id FROM experiment e INNER JOIN factor f ON e.id = f.experiment_id LEFT JOIN factor_properties_for_level fpfl ON f.id = fpfl.factor_id WHERE fpfl.id IS NULL LIMIT 10')
+    if (experiments && experiments.length) {
+      const experimentIds = experiments.map(e => e.id)
+      console.info('migrating experiment ids', experimentIds)
+      const factorLevels = await tx.any('SELECT fl.factor_id, value FROM factor f INNER JOIN (SELECT factor_id, MIN(id) as factor_level_id FROM factor_level GROUP BY factor_id) AS joiner ON f.id = joiner.factor_id INNER JOIN factor_level fl ON joiner.factor_level_id = fl.id WHERE f.experiment_id in ($1:csv)', [experimentIds])
+      const factorProperties = factorLevels.flatMap((factorLevel) => {
+        const factorLevelLines = getFactorLevelLines(factorLevel.value)[0]
+        return factorLevelLines
+          .map((detail, index) => ({
+            ...detail,
+            factorId: factorLevel.factor_id,
+            questionCode: detail.multiQuestionTag ? undefined : detail.questionCode,
+            columnNumber: index + 1,
+          }))
+      })
+      await createFactorProperties(factorProperties, tx)
+      return experimentIds.length
+    }
+    return 0
+  })
 
-const doLevelMigration = db =>
-  db.tx('propertyMigration', tx =>
-    tx.any('SELECT DISTINCT e.id FROM experiment e INNER JOIN factor f ON e.id = f.experiment_id INNER JOIN factor_level fl ON f.id = fl.factor_id LEFT JOIN factor_level_details fld ON fl.id = fld.factor_level_id WHERE fld.id IS NULL LIMIT 10')
-      .then((experiments) => {
-        if (experiments && experiments.length) {
-          const experimentIds = experiments.map(e => e.id)
-          console.info('migrating experiment ids', experimentIds)
-          return Promise.all([
-            tx.any('SELECT fl.id, factor_id, value FROM factor f INNER JOIN factor_level fl ON f.id = fl.factor_id WHERE f.experiment_id in ($1:csv)', [experimentIds]),
-            tx.any('SELECT factor_id, label, fpfl.id FROM factor f INNER JOIN factor_properties_for_level fpfl ON f.id = fpfl.factor_id WHERE experiment_id in ($1:csv)', [experimentIds]),
-          ]).then(([factorLevels, factorProperties]) => {
-            // do migration
-            const factorPropertyMap = factorProperties.reduce((map, factorProperty) => {
-              if (!map[factorProperty.factor_id]) {
-                map[factorProperty.factor_id] = {}
-              }
-              map[factorProperty.factor_id][factorProperty.label] = factorProperty.id
-              return map
-            }, {})
-            const factorLevelDetails = factorLevels.flatMap((factorLevel) => {
-              const factorLevelLines = getFactorLevelLines(factorLevel.value)
-              return factorLevelLines.flatMap((factorLevelLine, index) =>
-                factorLevelLine.map(detail => ({
-                  ...detail,
-                  factorLevelId: factorLevel.id,
-                  factorPropertiesForLevelId:
-                    factorPropertyMap[factorLevel.factor_id][detail.label],
-                  questionCode: detail.multiQuestionTag ? detail.questionCode : undefined,
-                  rowNumber: index + 1,
-                })))
-            })
-            return createFactorLevelDetails(factorLevelDetails, tx)
-          })
-            .then(() => experimentIds.length)
+// eslint-disable-next-line no-unused-vars
+const doLevelMigration = async db =>
+  db.tx('propertyMigration', async (tx) => {
+    const experiments = await tx.any('SELECT DISTINCT e.id FROM experiment e INNER JOIN factor f ON e.id = f.experiment_id INNER JOIN factor_level fl ON f.id = fl.factor_id LEFT JOIN factor_level_details fld ON fl.id = fld.factor_level_id WHERE fld.id IS NULL LIMIT 10')
+    if (experiments && experiments.length) {
+      const experimentIds = experiments.map(e => e.id)
+      console.info('migrating experiment ids', experimentIds)
+      const factorLevels = await tx.any('SELECT fl.id, factor_id, value FROM factor f INNER JOIN factor_level fl ON f.id = fl.factor_id WHERE f.experiment_id in ($1:csv)', [experimentIds])
+      const factorProperties = await tx.any('SELECT factor_id, label, fpfl.id FROM factor f INNER JOIN factor_properties_for_level fpfl ON f.id = fpfl.factor_id WHERE experiment_id in ($1:csv)', [experimentIds])
+      const factorPropertyMap = factorProperties.reduce((map, factorProperty) => {
+        if (!map[factorProperty.factor_id]) {
+          map[factorProperty.factor_id] = {}
         }
-        return 0
-      }))
-    .then((numberRowsMigrated) => {
-      if (numberRowsMigrated > 0) {
-        doLevelMigration(db)
-      }
-    })
+        map[factorProperty.factor_id][factorProperty.label] = factorProperty.id
+        return map
+      }, {})
+      const factorLevelDetails = factorLevels.flatMap((factorLevel) => {
+        const factorLevelLines = getFactorLevelLines(factorLevel.value)
+        return factorLevelLines.flatMap((factorLevelLine, index) =>
+          factorLevelLine.map(detail => ({
+            ...detail,
+            factorLevelId: factorLevel.id,
+            factorPropertiesForLevelId: factorPropertyMap[factorLevel.factor_id][detail.label],
+            questionCode: detail.multiQuestionTag ? detail.questionCode : undefined,
+            rowNumber: index + 1,
+          })))
+      })
+      await createFactorLevelDetails(factorLevelDetails, tx)
+      return experimentIds.length
+    }
+    return 0
+  })
 
 const createDbInstance = () =>
   configurator.init().then((config) => {
@@ -178,18 +160,34 @@ const createDbInstance = () =>
 
     pgp = pgPromise()
 
-    // Create the database instance with extensions:
     return pgp(dbConfig)
   })
 
+console.info(`
+By default, this code will create factor properties for every experiment in the database that does not have them yet.
+Creating these properties must be done before creating the level details.
+To switch to creating level details instead, search for and swap the "const migrationFunction" lines and then rerun the script.
+`)
 createDbInstance()
-  .then(db => doPropertyMigration(db))
-  .then(() => {
-    console.info('Migration completed successfully.')
-    process.exit(0)
-  })
-  .catch((err) => {
-    console.error('An error occurred during migration: ')
-    console.error(err)
-    process.exit(1)
+  .then(async (db) => {
+    // This is where you switch whether you migrate properties or level details:
+    const migrationFunction = doPropertyMigration
+    // const migrationFunction = doLevelMigration
+    let migratedExperimentsCount = 1
+    let totalExperimentsMigrated = 0
+
+    try {
+      while (migratedExperimentsCount > 0) {
+        migratedExperimentsCount = migrationFunction(db)
+        totalExperimentsMigrated += migratedExperimentsCount
+      }
+      console.info('Migration completed successfully.')
+      console.info(`A total of ${totalExperimentsMigrated} experiments where migrated successfully.`)
+      process.exit(0)
+    } catch (err) {
+      console.error('An error occurred during migration: ')
+      console.error(err)
+      console.info(`A total of ${totalExperimentsMigrated} experiments where migrated successfully before the error.`)
+      process.exit(1)
+    }
   })

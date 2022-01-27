ef3bc94ef3011201cc426f5b5b932f4823c21a48
Ricardo David Antonetti <ricardo.antonetti.ext@bayer.com>
2022-01-27 14:50:03 -0500

    EXP-916 Remove redundant test cases and fix typo

--- test/services/DesignSpecificationDetailService.test.js
+++ test/services/DesignSpecificationDetailService.test.js
@@ -277,7 +277,7 @@ describe('DesignSpecificationDetailService', () => {
   })
 
   describe('deleteInvalidSpecsForRandomization', () => {
-    test('calls deleteByExperimentAndKey with the correct key if the strategy does not allow borders', async () => {
+    test('calls deleteByExperimentAndKey with the correct key if the strategy does not allow borders nor rep buffers', async () => {
       target = new DesignSpecificationDetailService()
       dbRead.refDesignSpecification.all = mockResolve([
         { id: 2, name: 'Reps' },
@@ -293,7 +293,7 @@ describe('DesignSpecificationDetailService', () => {
       expect(dbWrite.designSpecificationDetail.deleteByExperimentAndKey).toHaveBeenCalledWith(5, [3, 5], testTx)
     })
 
-    test('does not call deleteExperimentAndKey if the strategy does allow borders', async () => {
+    test('does not call deleteExperimentAndKey if the strategy does allow borders and rep buffers', async () => {
       target = new DesignSpecificationDetailService()
       dbRead.refDesignSpecification.all = mockResolve([
         { id: 2, name: 'Reps' },
@@ -309,7 +309,7 @@ describe('DesignSpecificationDetailService', () => {
       expect(dbWrite.designSpecificationDetail.deleteByExperimentAndKey).not.toHaveBeenCalled()
     })
 
-    test('calls deleteByExperimentAndKey with the correct key if the strategy allows norders but does not allow rep buffers', async () => {
+    test('calls deleteByExperimentAndKey with the correct key if the strategy allows borders but does not allow rep buffers', async () => {
       target = new DesignSpecificationDetailService()
       dbRead.refDesignSpecification.all = mockResolve([
         { id: 2, name: 'Reps' },
@@ -323,38 +323,6 @@ describe('DesignSpecificationDetailService', () => {
       await target.deleteInvalidSpecsForRandomization(5, randomizationStrategy, testTx)
 
       expect(dbWrite.designSpecificationDetail.deleteByExperimentAndKey).toHaveBeenCalledWith(5, [5], testTx)
-    })
-
-    test('calls deleteByExperimentAndKey with the correct key if the strategy does not allow rep buffers', async () => {
-      target = new DesignSpecificationDetailService()
-      dbRead.refDesignSpecification.all = mockResolve([
-        { id: 2, name: 'Reps' },
-        { id: 3, name: 'Border Size' },
-        { id: 4, name: 'Locations' },
-        { id: 5, name: 'Rep Buffer Size' },
-      ])
-      dbWrite.designSpecificationDetail.deleteByExperimentAndKey = mockResolve()
-      const randomizationStrategy = { rules: {} }
-
-      await target.deleteInvalidSpecsForRandomization(5, randomizationStrategy, testTx)
-
-      expect(dbWrite.designSpecificationDetail.deleteByExperimentAndKey).toHaveBeenCalledWith(5, [3, 5], testTx)
-    })
-
-    test('does not call deleteExperimentAndKey if the strategy does allow rep buffers', async () => {
-      target = new DesignSpecificationDetailService()
-      dbRead.refDesignSpecification.all = mockResolve([
-        { id: 2, name: 'Reps' },
-        { id: 3, name: 'Border Size' },
-        { id: 4, name: 'Locations' },
-        { id: 5, name: 'Rep Buffer Size' },
-      ])
-      dbWrite.designSpecificationDetail.deleteByExperimentAndKey = mockResolve()
-      const randomizationStrategy = { rules: { buffers: { border: true, rep: true } } }
-
-      await target.deleteInvalidSpecsForRandomization(5, randomizationStrategy, testTx)
-
-      expect(dbWrite.designSpecificationDetail.deleteByExperimentAndKey).not.toHaveBeenCalled()
     })
   })
 })

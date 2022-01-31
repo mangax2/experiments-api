d45c7bf6c6e454e0e9dc4aa9249d659393745c37
Gorday, Jonathan G <jonathan.g.gorday@monsanto.com>
2022-01-31 13:56:11 -0600

    EXP-916: Fixed the index so it's on the correct columns.

--- migration-scripts/20220131_EXP-916_index.sql
+++ migration-scripts/20220131_EXP-916_index.sql
@@ -1,1 +1,1 @@
-CREATE INDEX design_spec_detail_id_ref_design_spec_id ON design_spec_detail (id, ref_design_spec_id);
+CREATE INDEX design_spec_detail_experiment_id_ref_design_spec_id ON design_spec_detail (experiment_id, ref_design_spec_id);

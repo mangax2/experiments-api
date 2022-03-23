d607fa204c4c1a85f61d245a829599adddf0fab1
EHZBZ <chandler.freeman.ext@bayer.com>
2022-03-23 12:16:52 -0400

    EXP-948 Allow nested overrides

--- src/configs/configurator.js
+++ src/configs/configurator.js
@@ -1,13 +1,18 @@
 const path = require('path')
 const fs = require('fs')
+const { merge } = require('lodash')
 const { Config, source, processor } = require('@monsantoit/config')
 
+const coreSourcePath = path.resolve(__dirname, './coreSource.js')
+const overridesPath = path.resolve(__dirname, './overrides.json')
 const sources = [
-  source.fromJS({ src: path.resolve(__dirname, './coreSource.js') }),
+  source.fromJS({ src: coreSourcePath }),
 ]
 
-if (process.env.NODE_ENV === 'development' && fs.existsSync('./src/config/overrides.json')) {
-  sources.push(source.fromFile({ src: path.resolve(__dirname, './overrides.json') }))
+if (process.env.NODE_ENV === 'development' && fs.existsSync('./src/configs/overrides.json')) {
+  const coreSources = require(coreSourcePath, 'utf8')
+  const overrides = JSON.parse(fs.readFileSync(overridesPath, 'utf8'))
+  sources.push(() => merge(coreSources, overrides))
 }
 
 module.exports = new Config({

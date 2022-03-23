a66a704ba15ac1d3da0df1b2e269a9b5eaeafbc9
EHZBZ <chandler.freeman.ext@bayer.com>
2022-03-23 12:24:40 -0400

    EXP-948 Fix require literal

--- src/configs/configurator.js
+++ src/configs/configurator.js
@@ -3,15 +3,13 @@ const fs = require('fs')
 const { merge } = require('lodash')
 const { Config, source, processor } = require('@monsantoit/config')
 
-const coreSourcePath = path.resolve(__dirname, './coreSource.js')
-const overridesPath = path.resolve(__dirname, './overrides.json')
 const sources = [
-  source.fromJS({ src: coreSourcePath }),
+  source.fromJS({ src: path.resolve(__dirname, './coreSource.js') }),
 ]
 
 if (process.env.NODE_ENV === 'development' && fs.existsSync('./src/configs/overrides.json')) {
-  const coreSources = require(coreSourcePath, 'utf8')
-  const overrides = JSON.parse(fs.readFileSync(overridesPath, 'utf8'))
+  const coreSources = require('./coreSource.js', 'utf8')
+  const overrides = JSON.parse(fs.readFileSync(path.resolve(__dirname, './overrides.json'), 'utf8'))
   sources.push(() => merge(coreSources, overrides))
 }
 

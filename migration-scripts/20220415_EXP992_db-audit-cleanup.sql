a80ec2517e8299b9ab24c889d7887fd23817f87a
tlamb <tong.lambert@monsanto.com>
2022-04-19 16:04:21 -0500

    EXP-992: migration script for db audit cleanup

---
+++ migration-scripts/20220415_EXP992_db-audit-cleanup.sql
@@ -0,0 +1,13 @@
+GRANT ALL PRIVILEGES ON SCHEMA audit TO GROUP exp_db_audit_cleanup_user;
+GRANT SELECT, DELETE ON ALL TABLES IN SCHEMA audit TO exp_db_audit_cleanup_user;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA audit TO exp_db_audit_cleanup_user;
+
+DROP TRIGGER IF EXISTS purge_graphql_audit ON audit.graphql_audit;
+
+DROP FUNCTION IF EXISTS process_graphql_audit();
+
+CREATE OR REPLACE PROCEDURE process_graphql_audit()
+    LANGUAGE SQL
+AS $$
+DELETE FROM audit.graphql_audit WHERE request_time < NOW() - INTERVAL '30 days';
+$$;

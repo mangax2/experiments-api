eb47fe301de5a1f86d3304bb3193fbe305bb17d0
EHZBZ <chandler.freeman.ext@bayer.com>
2022-10-11 11:39:29 -0400

    EXP-1118 Update Datadog and change load order

--- src/tracer.js
+++ src/tracer.js
@@ -1,14 +1,34 @@
-const tracer = require('dd-trace')
+const datadogEnvMap = {
+  dev: 'develop',
+  np: 'nonprod',
+  prod: 'prod',
+}
 
-tracer.init({
-  enabled: true,
-  logInjection: false,
-  debug: false,
-  analytics: true,
+const datadogEnv = datadogEnvMap[process.env.VAULT_ENV]
+const datadogService = `exp-api-${process.env.VAULT_ENV}`
+
+const tracer = require('dd-trace').init({
+  debug: true,
   runtimeMetrics: true,
+  env: datadogEnv,
+  service: datadogService,
 })
+
 tracer.use('http', {
   blocklist: ['/ping'],
+})
+
+tracer.use('express', {
+  hooks: {
+    request: (span, req) => {
+      if (req.context) {
+        span.setTag('request_id', req.context.requestId)
+        span.setTag('is_api_request', req.context.isApiRequest)
+        span.setTag('client.id', req.context.clientId)
+        span.setTag('client.user_id', req.context.userId)
+      }
+    },
+  },
 })
 
 module.exports = tracer
